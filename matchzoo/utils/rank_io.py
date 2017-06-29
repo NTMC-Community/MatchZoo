121e8f23fa30d939ea3543903c27c59cfcb6f94d
pl8787 <pl8787@qq.com>
2017-06-29 12:22:42 -0400

    input using network tags

--- matchzoo/utils/rank_io.py
+++ matchzoo/utils/rank_io.py
@@ -37,12 +37,13 @@ def read_data_old_version(filename):
     return data
 
 # Read Relation Data
-def read_relation(filename):
+def read_relation(filename, verbose=True):
     data = []
     for line in open(filename):
         line = line.strip().split()
         data.append( (int(line[0]), line[1], line[2]) )
-    print '[%s]\n\tInstance size: %s' % (filename, len(data))
+    if verbose:
+        print '[%s]\n\tInstance size: %s' % (filename, len(data))
     return data
 
 # Read Data Dict
