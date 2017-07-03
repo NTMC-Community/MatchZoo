adaa1879da6346392bbad390cd3286f8468e9d22
faneshion@148 <fanyixing111@hotmail.com>
2017-07-02 21:29:59 -0400

    drmm model

--- matchzoo/inputs/__init__.py
+++ matchzoo/inputs/__init__.py
@@ -2,5 +2,6 @@
 from .rank_data_generator import PairGenerator
 from .rank_data_generator import ListGenerator
 from .drmm_data_generator import DRMM_PairGenerator
-from ..utils import rank_io
+from .drmm_data_generator import DRMM_ListGenerator
+#from ..utils import rank_io
 
