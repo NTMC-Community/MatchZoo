3ae37cead6e9f4991d497307191717328999073a
faneshion@148 <fanyixing111@hotmail.com>
2017-07-09 23:21:36 -0400

    new models

--- matchzoo/models/dssm.py
+++ matchzoo/models/dssm.py
@@ -7,43 +7,43 @@ from keras.layers import Input, Embedding, Dense, Activation, Merge, Lambda, Per
 from keras.layers import Reshape, Dot
 from keras.activations import softmax
 
-def check(config):
-    ''' check config information for building
-    '''
-    def default_config(config):
-        return config
 
-    config = default_config(config)
-    check_list = [ 'text1_maxlen', 'text2_maxlen',
+class DSSM(BasicModel):
+    def __init__(self, config):
+        super(DSSM, self).__init__(config)
+        self.__name = 'DSSM'
+        self.check_list = [ 'text1_maxlen', 'text2_maxlen', 
                    'num_layers', 'hidden_sizes']
-    for e in check_list:
-        if e not in config:
-            print '[Model] Error %s not in config' % e
-            return False
-    return True
-def build(config):
-    def tensor_product(x):
-        a = x[0]
-        b = x[1]
-        y = K.batch_dot(a, b, axis=1)
-        y = K.einsum('ijk, ikl->ijl', a, b)
-        return y
-    query = Input(name='query', shape=(config['text1_maxlen'],))
-    doc = Input(name='doc', shape=(config['text2_maxlen']))
+        self.setup(config)
+        if not self.check():
+            raise TypeError('[DSSM] parameter check wrong')
+        print '[DSSM] init done'
 
-    def mlp_work(input_dim):
-        seq = Sequential()
-        seq.add(SparseFullyConnectedLayer(config['hidden_layers'][0], input_dim=input_dim, activation='relu'))
-        for i in range(config['num_layers']-1):
-            seq.add(Dense(config['hidden_sizes'][i+1], activation='relu'))
-        return seq
-        
-    assert config['text1_maxlen'] == config['text2_maxlen']
-    mlp = mlp_work(config['text1_maxlen'])
-    rq = mlp(query)
-    rd = mlp(doc)
-    out_ = Merge([rq, rd], mode='cos', dot_axis=1)
-    #out_ = Dot( axes= [1, 1])([rq, rd])
+    def setup(self, config):
+        if not isinstance(config, dict):
+            raise TypeError('parameter config should be dict:', config)
+            
+        self.set_default('num_layers', 2)
+        self.set_default('hidden_sizes', [300, 128])
+        self.config.update(config)
 
-    model = Model(inputs=[query, doc], outputs=[out_])
-    return model
+    def build(self):
+        query = Input(name='query', shape=(self.config['text1_maxlen'],))
+        doc = Input(name='doc', shape=(self.config['text2_maxlen']))
+
+        def mlp_work(input_dim):
+            seq = Sequential()
+            seq.add(SparseFullyConnectedLayer(self.config['hidden_layers'][0], input_dim=input_dim, activation='relu'))
+            for i in range(self.config['num_layers']-1):
+                seq.add(Dense(self.config['hidden_sizes'][i+1], activation='relu'))
+            return seq
+            
+        assert self.config['text1_maxlen'] == self.config['text2_maxlen']
+        mlp = mlp_work(self.config['text1_maxlen'])
+        rq = mlp(query)
+        rd = mlp(doc)
+        out_ = Merge([rq, rd], mode='cos', dot_axis=1)
+        #out_ = Dot( axes= [1, 1])([rq, rd])
+
+        model = Model(inputs=[query, doc], outputs=[out_])
+        return model
