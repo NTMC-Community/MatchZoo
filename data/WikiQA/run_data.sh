c11ac36c3376ebab979867c92e1d31e141798c1a
lyang <yangliuyx@gmail.com>
2017-12-02 19:56:42 -0500

    finish add model anmm into matchzoo

--- data/WikiQA/run_data.sh
+++ data/WikiQA/run_data.sh
@@ -23,9 +23,11 @@ python norm_embed.py embed_glove_d300 embed_glove_d300_norm
 python gen_w2v.py  glove.6B.50d.txt word_dict.txt embed_glove_d50
 python norm_embed.py embed_glove_d50 embed_glove_d50_norm
 
-# generate data for drmm, these commands are used for generating histograms only for drmm model.
+# generate data histograms for drmm model
+# generate data bin sums for anmm model
 # generate idf file
 cat word_stats.txt | cut -d ' ' -f 1,4 > embed.idf
 python gen_hist4drmm.py 60
+python gen_binsum4anmm.py 20 # the default number of bin is 20
 
 echo "Done ..."
