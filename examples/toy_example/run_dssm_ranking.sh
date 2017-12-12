b3233dafba6d605ee807a02ba069c84b2804ce4c
faneshion@71 <fanyixing111@hotmail.com>
2017-12-13 00:59:07 +0800

    triletter generator bug fixed

--- examples/toy_example/run_dssm_ranking.sh
+++ examples/toy_example/run_dssm_ranking.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/toy_example/config/arci_ranking.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/toy_example/config/dssm_ranking.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/toy_example/config/arci_ranking.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/toy_example/config/dssm_ranking.config
