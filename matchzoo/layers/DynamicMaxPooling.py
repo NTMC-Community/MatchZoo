02918aa0ef762efafacc75ece4c166567ada3f84
pl8787 <pl8787@gmail.com>
2018-05-29 07:09:28 -0400

    [DynamicPooling] fix size errors. Add coverage to gitignore.

--- matchzoo/layers/DynamicMaxPooling.py
+++ matchzoo/layers/DynamicMaxPooling.py
@@ -11,11 +11,10 @@ class DynamicMaxPooling(Layer):
         super(DynamicMaxPooling, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        #self.dpool_index = Input(name='dpool_index', shape=[input_shape[1], input_shape[2], 3], dtype='int32')
         input_shape_one = input_shape[0]
         self.msize1 = input_shape_one[1]
         self.msize2 = input_shape_one[2]
-        super(DynamicMaxPooling, self).build(input_shape)  # Be sure to call this somewhere!
+        super(DynamicMaxPooling, self).build(input_shape)  
 
     def call(self, data):
         x, self.dpool_index = data
@@ -26,9 +25,11 @@ class DynamicMaxPooling(Layer):
         suggestion1 = self.msize1 / stride1
         suggestion2 = self.msize2 / stride2
 
-        if suggestion1!=self.psize1 or suggestion2!=self.psize2:
-            print("DynamicMaxPooling Layer can not generate (%s x %s) output feature map,"
-                "please use (%s x %s instead.)" % (self.psize1, self.psize2, suggestion1, suggestion2))
+        if suggestion1 != self.psize1 or suggestion2 != self.psize2:
+            print("DynamicMaxPooling Layer can not "
+                  "generate ({} x {}) output feature map,"
+                  "please use ({} x {} instead.)".format(self.psize1, self.psize2, 
+                                                       suggestion1, suggestion2))
             exit()
 
         x_pool = K.tf.nn.max_pool(x_expand, 
@@ -42,14 +43,22 @@ class DynamicMaxPooling(Layer):
         return (None, self.psize1, self.psize2, input_shape_one[3])
 
     @staticmethod
-    def dynamic_pooling_index(len1, len2, max_len1, max_len2, compress_ratio1 = 1, compress_ratio2 = 1):
+    def dynamic_pooling_index(len1, len2, max_len1, max_len2, 
+                              compress_ratio1 = 1, compress_ratio2 = 1):
         def dpool_index_(batch_idx, len1_one, len2_one, max_len1, max_len2):
+            if len1_one == 0:
+                print("len1 = 0 at batch_idx = {}".format(batch_idx))
+                exit()
+            if len2_one == 0:
+                print("len2 = 0 at batch_idx = {}".format(batch_idx))
+                exit()
             stride1 = 1.0 * max_len1 / len1_one
             stride2 = 1.0 * max_len2 / len2_one
-            idx1_one = [int(i/stride1) for i in range(max_len1)]
-            idx2_one = [int(i/stride2) for i in range(max_len2)]
+            idx1_one = [int(i / stride1) for i in range(max_len1)]
+            idx2_one = [int(i / stride2) for i in range(max_len2)]
             mesh1, mesh2 = np.meshgrid(idx1_one, idx2_one)
-            index_one = np.transpose(np.stack([np.ones(mesh1.shape) * batch_idx, mesh1, mesh2]), (2,1,0))
+            index_one = np.transpose(np.stack([np.ones(mesh1.shape) * batch_idx,
+                                      mesh1, mesh2]), (2,1,0))
             return index_one
         index = []
         dpool_bias1 = dpool_bias2 = 0
@@ -57,8 +66,9 @@ class DynamicMaxPooling(Layer):
             dpool_bias1 = 1
         if max_len2 % compress_ratio2 != 0:
             dpool_bias2 = 1
-        cur_max_len1 = max_len1//compress_ratio1+dpool_bias1
-        cur_max_len2 = max_len2//compress_ratio2+dpool_bias2
+        cur_max_len1 = max_len1 // compress_ratio1 + dpool_bias1
+        cur_max_len2 = max_len2 // compress_ratio2 + dpool_bias2
         for i in range(len(len1)):
-            index.append(dpool_index_(i, len1[i]//compress_ratio1, len2[i]//compress_ratio2, cur_max_len1, cur_max_len2))
+            index.append(dpool_index_(i, len1[i] // compress_ratio1, 
+                         len2[i] // compress_ratio2, cur_max_len1, cur_max_len2))
         return np.array(index)
