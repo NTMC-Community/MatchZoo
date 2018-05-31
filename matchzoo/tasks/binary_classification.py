3a2f0271376a73849f6276d0ccc63f09a23df1f8
uduse <i@uduse.com>
2018-05-31 14:43:30 +0800

    Update docstrings

--- matchzoo/tasks/binary_classification.py
+++ matchzoo/tasks/binary_classification.py
@@ -1,13 +1,17 @@
+"""Binary classification task."""
+
 from matchzoo import engine
 
 
 class BinaryClassification(engine.BaseTask):
-    """Classification Task."""
+    """Classification task."""
 
     @classmethod
     def list_available_losses(cls) -> list:
+        """Return a list of available losses."""
         return ['binary_crossentropy']
 
     @classmethod
     def list_available_metrics(cls) -> list:
+        """Return a list of available metrics."""
         return ['acc']
