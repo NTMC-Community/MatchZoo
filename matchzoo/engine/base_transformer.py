4365fd52db1ded3da9a68bc98cefd8179f42f697
faneshion <fanyixing111@hotmail.com>
2018-06-01 09:38:55 +0800

    base_transformer docstring

--- matchzoo/engine/base_transformer.py
+++ matchzoo/engine/base_transformer.py
@@ -1,12 +1,11 @@
-"""Contains the base Transformer class, from which all transformers."""
+"""Contains the base Transformer class for  all transformers."""
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
 
 
 class TransformerMiXin(object):
-    """MiXin class for all transformers in MatchZoo. This class is copyed from
-    scikit-learn.
+    """MiXin class for all transformers in MatchZoo.
 
     # Methods:
         fit_transform(X, y=None, **fit_params)
@@ -14,11 +13,10 @@ class TransformerMiXin(object):
     """
 
     def fit_transform(self, X, y=None, **fit_params):
-
         """Fit to data, then transform it.
 
-        Fits transform to X and y with optional parameters fit_params and
-        returns a transformed version of X.
+        Fits transform to X and y with optional parameters fit_params.
+        Then, returns a transformed version of X.
 
         Parameters
         -----------
@@ -32,6 +30,7 @@ class TransformerMiXin(object):
         -------
         X_new : numpy array of shape [n_samples, n_pairs]
             Transformed array.
+
         """
         if y is None:
             # fit method of arity 1 (unsuperised transformation)
