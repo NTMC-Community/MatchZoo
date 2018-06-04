ba066439d067c7dd120c08757a5a64fdea6f0f64
uduse <i@uduse.com>
2018-06-04 21:17:59 +0800

    Fill missing docstrings, and minor format fixes.

--- matchzoo/tasks/classification.py
+++ matchzoo/tasks/classification.py
@@ -9,6 +9,7 @@ class Classification(engine.BaseTask):
     """Classification task."""
 
     def __init__(self, num_classes: int = 2):
+        """Classification task."""
         super().__init__()
         if not isinstance(num_classes, int):
             raise TypeError("Number of classes must be an integer.")
@@ -18,17 +19,19 @@ class Classification(engine.BaseTask):
 
     @property
     def num_classes(self) -> int:
+        """:return: number of classes to classify."""
         return self._num_classes
 
     @classmethod
     def list_available_losses(cls) -> list:
-        """Return a list of available losses."""
+        """:return: a list of available losses."""
         return ['categorical_crossentropy']
 
     @classmethod
     def list_available_metrics(cls) -> list:
-        """Return a list of available metrics."""
+        """:return: a list of available metrics."""
         return ['acc']
 
-    def make_output_layer(self):
+    def make_output_layer(self) -> keras.layers.Dense:
+        """:return: a correctly shaped keras dense layer for model output."""
         return keras.layers.Dense(self._num_classes, activation='softmax')
