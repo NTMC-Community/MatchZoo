6b17f6e23b5064ddc4d8da57841cf54c45b8a52f
bo <kingbolanda@live.com>
2018-06-11 21:46:20 +0200

    add lancaster stemmer option

--- tests/unit_test/test_preprocessors.py
+++ tests/unit_test/test_preprocessors.py
@@ -1,14 +1,16 @@
 from matchzoo.preprocessors import *
-
+import pytest
+import functools
 
 def test_preprocessors():
+    test_text = "This is an Example sentence to BE ! cleaned with digits 31."
     processor = chain(tokenizer,
                       to_lowercase,
                       remove_punctuation,
                       remove_digits,
                       stemming,
                       lemmatization)
-    rv = processor("This is an Example sentence to BE ! cleaned with digits 31.")
+    rv = processor(test_text)
     assert len(rv) == 10
     stoplist = get_stopwords('zh')
     assert len(stoplist) == 119
@@ -17,3 +19,11 @@ def test_preprocessors():
     sent = '这是一个测试句子．'
     segmented = segmentation(sent)
     assert len(segmented) == 5
+    processor = chain(tokenizer,
+                      to_lowercase,
+                      functools.partial(stemming, stemmer='lancaster'))
+    rv = processor(test_text)
+    assert 'thi' in rv
+    with pytest.raises(ValueError):
+        stemming(test_text, 'fakestemmer')
+                
