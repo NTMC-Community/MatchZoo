0936666a5d6c5341588ed121bd84706fa7b8147c
uduse <i@uduse.com>
2018-06-13 00:57:10 +0800

    Add hyper spaces docstrings

--- matchzoo/engine/hyper_spaces.py
+++ matchzoo/engine/hyper_spaces.py
@@ -1,3 +1,35 @@
+"""
+Hyper parameter search spaces wrapping `hyperopt`.
+
+See `hyperopt`'s documentation for more details:
+https://github.com/hyperopt/hyperopt/wiki/FMin
+
+Reason of these wrappers:
+
+    A hyper space in `hyperopt` requires a `label` to instantiate. This `label`
+    is used later as a reference to original hyper space that is sampled. In
+    `matchzoo`, hyper spaces are used in :class:`matchzoo.engine.Param`s. Only
+    if a hyper space's label matches its parent
+    :class:`matchzoo.engine.Param`'s name, `matchzoo` can correctly
+    back-refrenced the parameter got sampled. This can be done by asking the
+    user always use the same name for a parameter and its hyper space, but
+    typos can occur. As a result, these wrappers are created to hide hyper
+    spaces' `label`s, and always correctly bind them with its parameter's
+    name.
+
+Example:
+
+    >>> from hyperopt.pyll.stochastic import sample
+    >>> numbers = [0, 1, 2]
+    >>> sample(choice(options=numbers)('numbers')) in numbers
+    True
+    >>> 0 <= sample(quniform(low=0, high=9)('digit')) <= 9
+    True
+    >>> 0 <= sample(uniform(low=0, high=9)('digit')) <= 9
+    True
+
+"""
+
 import functools
 
 from hyperopt import hp
@@ -26,3 +58,5 @@ def uniform(low, high) -> functools.partial:
 @_hyperopt_api
 def quniform(low, high, q=1) -> functools.partial:
     return functools.partial(hp.quniform, low=low, high=high, q=q)
+
+# TODO: more API
