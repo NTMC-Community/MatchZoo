94bfaccd8b035b8bc41a476268d8ed3a9cd35dba
uduse <i@uduse.com>
2018-06-14 11:22:17 +0800

    Update docstrings and Tuner class. Pass all tests locally.

--- matchzoo/engine/param_table.py
+++ matchzoo/engine/param_table.py
@@ -1,4 +1,4 @@
-"""Model parametrs."""
+"""Parameters table class."""
 
 import typing
 
@@ -25,16 +25,18 @@ class ParamTable(object):
     """
 
     def __init__(self):
+        """Parameter table constrctor."""
         self._params = {}
 
     def add(self, param: Param):
-        """:param param: parameter to add"""
+        """:param param: parameter to add."""
         if not isinstance(param, Param):
             raise TypeError("Only accepts Param instance.")
         self._params[param.name] = param
 
     @property
     def hyper_space(self) -> dict:
+        """:return: Hyper space of the table, a valid `hyperopt` graph."""
         return {
             param.name: param.hyper_space
             for param in self._params.values()
@@ -42,14 +44,23 @@ class ParamTable(object):
         }
 
     def __getitem__(self, key: str) -> typing.Any:
+        """:return: A parameter instance in the table named `key`."""
         return self._params[key].value
 
     def __setitem__(self, key: str, value: typing.Any):
+        """
+        Set the value of the parameter named `key`.
+
+        :param key: Name of the parameter.
+        :param value: New value of the parameter to set.
+        """
         self._params[key].value = value
 
     def __str__(self):
+        """:return: Pretty formatted parameter table."""
         return '\n'.join(param.name.ljust(30) + str(param.value)
                          for param in self._params.values())
 
     def __iter__(self) -> typing.Iterator:
+        """:return: A iterator that iterates over all parameter instances."""
         yield from self._params.values()
--- matchzoo/engine/tuner.py
+++ matchzoo/engine/tuner.py
@@ -1,3 +1,5 @@
+"""Tuner class. Currently a minimum working demo."""
+
 import hyperopt
 import random
 
@@ -13,42 +15,50 @@ class Tuner(object):
     iteratively sample parameters and test for loss, and pick the best trial
     out of all.
 
-    :param model: Model to tune.
-    :param max_evals: Number of evaluations of a single tuning process.
+    Currently a minimum working demo.
 
     Example:
 
         >>> from matchzoo.models import DenseBaselineModel
         >>> model = DenseBaselineModel()
         >>> max_evals = 4
-        >>> tuner = Tuner(model, max_evals=max_evals)
-        >>> trials = tuner.tune()
+        >>> tuner = Tuner(model)
+        >>> trials = tuner.tune(max_evals)
         >>> len(trials) == max_evals
         True
 
     """
 
-    def __init__(self, model: engine.BaseModel, max_evals=32):
+    def __init__(self, model: engine.BaseModel):
+        """
+        Tuner constructor.
+
+        :param model: Model to tune.
+        """
         self._model = model
-        self._max_evals = max_evals
 
-    def tune(self):
+    def tune(self, max_evals: int = 32) -> list:
+        """
+        Tune the binded model `max_evals` times.
 
-        def test_wrapper(space):
-            for key, value in space.items():
-                self._model.params[key] = value
-            self._model.guess_and_fill_missing_params()
-            self._model.build()
-            # TODO: use model fit loss instead of a random loss
-            return {'loss'  : random.random(), 'space': space,
-                    'status': hyperopt.STATUS_OK}
-
+        :param max_evals: Number of evaluations of a single tuning process.
+        :return: A list of trials of the tuning process.
+        """
         trials = hyperopt.Trials()
         hyperopt.fmin(
-                fn=test_wrapper,
+                fn=self._test_wrapper,
                 space=self._model.params.hyper_space,
                 algo=hyperopt.tpe.suggest,
-                max_evals=self._max_evals,
+                max_evals=max_evals,
                 trials=trials
         )
         return trials.trials
+
+    def _test_wrapper(self, space):
+        for key, value in space.items():
+            self._model.params[key] = value
+        self._model.guess_and_fill_missing_params()
+        self._model.build()
+        # TODO: use model fit loss instead of a random loss
+        return {'loss':   random.random(), 'space': space,
+                'status': hyperopt.STATUS_OK}
