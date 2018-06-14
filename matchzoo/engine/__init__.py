1ad721393a103289bb95a9db535db805c9ec96e9
uduse <i@uduse.com>
2018-06-14 11:34:28 +0800

    Replace class-based tuner with a single function.

--- matchzoo/engine/__init__.py
+++ matchzoo/engine/__init__.py
@@ -3,4 +3,4 @@ from .param import Param
 from .param_table import ParamTable
 from .base_task import BaseTask, list_available_tasks
 from .base_model import BaseModel, load_model
-from .tuner import Tuner
+from .tune import tune
