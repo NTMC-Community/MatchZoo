b824b2984e5bbddf992ebee7c3593b6f4e4b9cf8
bo <kingbolanda@live.com>
2018-06-19 09:34:28 +0200

    remove resouce package (os denpendent), use psutil

--- matchzoo/utils/utility.py
+++ matchzoo/utils/utility.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import traceback
-import resource
+import psutil
 
 
 def show_layer_info(layer_name, layer_out):
@@ -10,13 +10,8 @@ def show_layer_info(layer_name, layer_out):
 
 
 def show_memory_use():
-    rusage_denom = 1024.
-    if sys.platform == 'darwin':
-        rusage_denom = rusage_denom * rusage_denom
-    ru = resource.getrusage(resource.RUSAGE_SELF)
-    total_memory = 1. * (ru.ru_maxrss + ru.ru_ixrss + ru.ru_idrss + ru.ru_isrss) / rusage_denom
-    strinfo = "\x1b[33m [Memory] Total Memory Use: %.4f MB \t Resident: %ld Shared: %ld UnshareData: %ld UnshareStack: %ld \x1b[0m" % \
-        (total_memory, ru.ru_maxrss, ru.ru_ixrss, ru.ru_idrss, ru.ru_isrss)
+    used_memory_percent = psutil.virtual_memory().percent
+    strinfo = '{}% memory has been used'.format(used_memory_percent)
     return strinfo
 
 
