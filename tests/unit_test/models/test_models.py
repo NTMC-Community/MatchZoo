fae74d8cb64351e5a116f2640615a84f80f440c4
uduse <i@uduse.com>
2018-06-22 20:19:51 +0800

    Add standardized model testing

---
+++ tests/unit_test/models/test_models.py
@@ -0,0 +1,80 @@
+import numpy as np
+import shutil
+
+import pytest
+
+from matchzoo import engine
+from matchzoo import models
+from matchzoo import tasks
+
+model_setups = [
+    (models.NaiveModel, None),
+    (models.DenseBaselineModel, None),
+    (models.DenseBaselineModel, {"num_dense_units": 512}),
+]
+
+
+@pytest.fixture(scope='module', params=[1, 32])
+def num_samples(request):
+    return request.param
+
+
+@pytest.fixture(scope='module', params=[
+    tasks.Classification(num_classes=2),
+    tasks.Classification(num_classes=16),
+    tasks.Ranking()
+])
+def task(request):
+    return request.param
+
+
+@pytest.fixture(params=model_setups)
+def raw_model(request):
+    model_class, custom_kwargs = request.param
+    model = model_class()
+    if custom_kwargs:
+        for key, val in custom_kwargs.items():
+            model.params[key] = val
+    return model
+
+
+@pytest.fixture
+def compiled_model(raw_model, task):
+    raw_model.params['task'] = task
+    raw_model.guess_and_fill_missing_params()
+    raw_model.build()
+    raw_model.compile()
+    return raw_model
+
+
+@pytest.fixture
+def x(compiled_model, num_samples):
+    input_shapes = compiled_model.params['input_shapes']
+    return [np.random.randn(num_samples, *shape) for shape in input_shapes]
+
+
+@pytest.fixture
+def y(compiled_model, num_samples):
+    task = compiled_model.params['task']
+    return np.random.randn(num_samples, *task.output_shape)
+
+
+def test_model_fit(compiled_model, x, y):
+    assert compiled_model.fit(x, y, verbose=0)
+
+
+def test_model_evaluate(compiled_model, x, y):
+    assert compiled_model.evaluate(x, y, verbose=0)
+
+
+def test_model_predict(compiled_model, x):
+    assert compiled_model.predict(x) is not None
+
+
+def test_save_load_model(compiled_model):
+    tmpdir = '.tmpdir'
+    compiled_model.save(tmpdir)
+    assert engine.load_model(tmpdir)
+    with pytest.raises(FileExistsError):
+        compiled_model.save(tmpdir)
+    shutil.rmtree(tmpdir)
