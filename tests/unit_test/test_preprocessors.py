c6b88273f295d2003fa72b5684e85bdd0b4af333
bo <kingbolanda@live.com>
2018-06-25 15:09:30 +0200

    refactor stateful unit, update nletterprocessunit, update unittest

--- tests/unit_test/test_preprocessors.py
+++ tests/unit_test/test_preprocessors.py
@@ -1,39 +1,60 @@
 from matchzoo.preprocessors import *
 import pytest
 
-def test_preprocessors():
-    test_text = "This is an Example sentence to BE ! cleaned with digits 31."
+@pytest.fixture
+def raw_input():
+    return "This is an Example sentence to BE ! cleaned with digits 31."
+
+@pytest.fixture
+def list_input():
+    return ['this', 'Is', 'a', 'the', 'test', 'lIst', '36', '!', 'input']
+
+def test_tokenize_unit(raw_input):
     tu = TokenizeUnit()
-    out = tu.transform(test_text)
+    out = tu.transform(raw_input)
     assert len(out) == 13
     assert 'an' in out
+
+def test_lowercase_unit(list_input):
     lu = LowercaseUnit()
-    out = lu.transform(out)
-    assert len(out) == 13
-    assert 'be' in out
+    out = lu.transform(list_input)
+    assert 'is' in out
+
+def test_digitremoval_unit(list_input):
     du = DigitRemovalUnit()
-    out = du.transform(out)
-    assert len(out) == 12
-    assert '31' not in out
+    out = du.transform(list_input)
+    assert 36 not in out
+
+def test_puncremoval_unit(list_input):
     pu = PuncRemovalUnit()
-    out = pu.transform(out)
-    assert len(out) == 10
+    out = pu.transform(list_input)
     assert '!' not in out
-    stop_u = StopRemovalUnit()
-    out = stop_u.transform(out)
-    assert len(out) == 3
+
+def test_stopremoval_unit(list_input):
+    su = StopRemovalUnit()
+    out = su.transform(list_input)
+    assert 'the' not in out
+
+def test_stemming_unit(list_input):
     su_porter = StemmingUnit()
-    out_porter = su_porter.transform(out)
-    assert 'sentenc' in out_porter
+    out_porter = su_porter.transform(list_input)
+    assert 'thi' in out_porter
     su_lancaster = StemmingUnit(stemmer='lancaster')
-    out_lancaster = su_lancaster.transform(out)
-    assert 'cle' in out_lancaster
-    su_error = StemmingUnit(stemmer='fake')
+    out_lancaster = su_lancaster.transform(list_input)
+    assert 'thi' in out_lancaster
+    su_not_exist = StemmingUnit(stemmer='fake_stemmer')
     with pytest.raises(ValueError):
-        su_error.transform(out)
-    leu = LemmatizationUnit()
-    out = leu.transform(out_porter)
-    assert len(out) == 3
-    StatefulProcessorUnit.__abstractmethods__=set()
-    stateful_unit = StatefulProcessorUnit()
-    assert stateful_unit.state == {}
+        su_not_exist.transform(list_input)
+
+def test_lemma_unit(list_input):
+    lemma = LemmatizationUnit()
+    out = lemma.transform(list_input)
+    assert 'this' in out
+
+def test_ngram_unit(list_input):
+    ngram = NgramLetterUnit()
+    out = ngram.transform(list_input)
+    context = ngram.state
+    print(context)
+    assert '#a#' in out
+    assert context.get('input_dim') == 24
