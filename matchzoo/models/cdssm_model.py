02ff4c4ac16983387518257af77e1ddb43c85200
bo <kingbolanda@live.com>
2018-06-28 01:19:21 +0200

    add cdssm

--- matchzoo/models/cdssm_model.py
+++ matchzoo/models/cdssm_model.py
@@ -1,7 +1,7 @@
 """An implementation of CDSSM, Deep Structured Semantic Model."""
 from matchzoo import engine
 from keras.models import Model
-from keras.layers import Dense, Input, Dot, Conv1D
+from keras.layers import Dense, Input, Dot, Conv1D, Maximum
 from keras import backend as K
 
 
@@ -23,7 +23,7 @@ class CDSSMModel(engine.BaseModel):
         params['optimizer'] = 'sgd'
         # TODO GET TRI-LETTER DIMENSIONALITY FROM FIT-TRANSFORM AS INPUT SHAPE
         # Dimension: (Contextual Sliding Window , NUM_TRI_LETTERS)
-        params['input_shapes'] = [(90000, 100), (90000, 500)]
+        params['input_shapes'] = [(90000, 10), (90000, 50)]
         params.add(engine.Param('w_initializer', 'glorot_normal'))
         params.add(engine.Param('b_initializer', 'zeros'))
         params.add(engine.Param('dim_fan_out', 128))
@@ -41,23 +41,38 @@ class CDSSMModel(engine.BaseModel):
         CDSSM use Siamese arthitecture.
         """
         # Left input and right input.
-        input_shape_le = self._params['input_shapes'][0]
-        input_shape_ri = self._params['input_shapes'][1]
-        input_le = Input(shape=input_shape_le)
-        input_ri = Input(shape=input_shape_ri)
+        dim_triletter = self._params['input_shapes'][0][0]
+        num_tri_letters_le = self._params['input_shapes'][0][1]
+        num_tri_letters_ri = self._params['input_shapes'][1][1]
+        input_shape_le = (dim_triletter, num_tri_letters_le)
+        input_shape_ri = (dim_triletter, num_tri_letters_ri)
+        # Create documen level network (Max-pooling and Dense).
+        document_level_network_le = self._document_level_max_pooling(
+            input_shape_le)
+        document_level_network_ri = self._document_level_max_pooling(
+            input_shape_ri)
+        # Create inputs.
+        inputs_le = [Input(shape=(dim_triletter, num_tri_letters_le))
+                    for _
+                    in range(num_tri_letters_le)]
+                       
+        inputs_ri = [Input(shape=(dim_triletter, num_tri_letters_ri))
+                    for _
+                    in range(num_tri_letters_ri)]
         # Process left & right input.
-        x = [self._document_level_max_pooling(input_shape_le[1]),
-             self._document_level_max_pooling(input_shape_ri[1])]
+        inputs_le = document_level_network_le(inputs_le)
+        inputs_ri = document_level_network_ri(inputs_ri)
+        x = [inputs_le, inputs_ri]
         # Dot product with cosine similarity.
         x = Dot(axes=[1, 1],
                 normalize=True)(x)
         # Make output layer and return Model.
         x_out = self._make_output_layer()(x)
         self._backend = Model(
-            inputs=[input_le, input_ri],
+            inputs=[inputs_le, inputs_ri],
             outputs=x_out)
 
-    def _triletter_level_conv(self) -> Model:
+    def _triletter_level_conv(self, input_shape) -> Model:
         """
         Apply convolutional operation towards to each tri-letter.
 
@@ -65,7 +80,6 @@ class CDSSMModel(engine.BaseModel):
         """
         # TODO use sparse input in the future.
         # Input word hashing layer.
-        input_shape = (self._params['input_shapes'][0][0],)
         input = Input(shape=input_shape)
         # Convolutional layer.
         x = Conv1D(filters=self._params['dim_conv'],
@@ -76,20 +90,31 @@ class CDSSMModel(engine.BaseModel):
                    bias_initializer=self._params['b_initializer'])(input)
         return Model(inputs=input, outputs=x)
     
-    def _document_level_max_pooling(self, num_tri_letters: int) -> Model:
+    def _document_level_max_pooling(self, input_shape: tuple) -> Model:
         """
         Apply Max-pooling on document (N Tri-letters).
         """
+        # Apply conv on each 90k dim tri-letter.
+        # Result in a (num_triletters * 300) matrix.
+        num_tri_letters = input_shape[1]
+        tri_letter_input_shape = (
+            self._params['input_shapes'][0][0],
+            num_tri_letters)
+        tri_letter_level_network = self._triletter_level_conv(
+            input_shape=tri_letter_input_shape)
+        # Dynamically created inputs.
         tri_letter_inputs = []
         for idx in range(num_tri_letters):
             tri_letter_inputs.append(
-                Input(shape=(self._params['dim_conv'],)))
-        # Apply conv on each 90k dim tri-letter.
-        # Result in a (num_triletters * 300) matrix.
-        x =  K.map_fn(self._triletter_level_conv, tri_letter_inputs)
+                Input(shape=tri_letter_input_shape))
+        # Process tri-letter inputs
+        x = [tri_letter_level_network(tri_letter_input)
+             for tri_letter_input
+             in tri_letter_inputs]
         # Take max at each dimension across all word-trigram features.
         # Result in a (1 * 300) tensor representation.
-        x = K.max(axis=0, keepdims=True)(x)
+        # TODO Make sure element-wise maximum is correct.
+        x = Maximum()(x)
         # Use Dense Layer to reduce 300 d to 128 d.
         x_out = Dense(self._params['dim_fan_out'],
                       activation=self._params['activation_dense'])(x)
