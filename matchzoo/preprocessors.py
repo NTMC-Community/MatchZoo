bd8444d78ee52a78cc93d1d9aa25e3d8006c5600
uduse <i@uduse.com>
2018-07-02 17:44:25 +0800

    Refactor vocabulary class. Add doc strings.

--- matchzoo/preprocessors.py
+++ matchzoo/preprocessors.py
@@ -230,55 +230,68 @@ class NgramLetterUnit(StatefulProcessorUnit):
         self._state = {'input_dim': input_dim}
 
 
-
 class Vocabulary(StatefulProcessorUnit):
     """
+    Vocabulary class.
+
+    Examples:
         >>> vocab = Vocabulary()
         >>> vocab.fit(['A', 'B', 'C', 'D', 'E'])
-        >>> token_index = vocab.state['token_index']
-        >>> token_index  # doctest: +SKIP
+        >>> term_index = vocab.state['term_index']
+        >>> term_index  # doctest: +SKIP
         {'E': 1, 'C': 2, 'D': 3, 'A': 4, 'B': 5}
-        >>> index_token = vocab.state['index_token']
-        >>> index_token  # doctest: +SKIP
+        >>> index_term = vocab.state['index_term']
+        >>> index_term  # doctest: +SKIP
         {1: 'C', 2: 'A', 3: 'E', 4: 'B', 5: 'D'}
 
-        >>> token_index['out-of-vocabulary-token']
+        >>> term_index['out-of-vocabulary-term']
         0
-        >>> index_token[0]
+        >>> index_term[0]
         ''
-        >>> index_token[42]
+        >>> index_term[42]
         Traceback (most recent call last):
             ...
         KeyError: 42
 
-        >>> a_index = token_index['A']
-        >>> c_index = token_index['C']
+        >>> a_index = term_index['A']
+        >>> c_index = term_index['C']
         >>> vocab.transform(['C', 'A', 'C']) == [c_index, a_index, c_index]
         True
         >>> vocab.transform(['C', 'A', 'OOV']) == [c_index, a_index, 0]
         True
 
+        >>> indices = vocab.transform('ABCDDZZZ')
+        >>> ''.join(vocab.state['index_term'][i] for i in indices)
+        'ABCDD'
+
     """
 
-    class IndexToken(dict):
+    class IndexTerm(dict):
+        """Map index to term."""
+
         def __missing__(self, key):
+            """Map out-of-vocabulary indices to empty string."""
             if key == 0:
                 return ''
             else:
                 raise KeyError(key)
 
-    class TokenIndex(dict):
+    class TermIndex(dict):
+        """Map term to index."""
+
         def __missing__(self, key):
+            """Map out-of-vocabulary terms to index 0."""
             return 0
 
     def fit(self, tokens: list):
-        token_index = self.TokenIndex()
-        index_token = self.IndexToken()
-        for index, token in enumerate(set(tokens)):
-            token_index[token] = index + 1
-            index_token[index + 1] = token
-        self._state['token_index'] = token_index
-        self._state['index_token'] = index_token
+        """Build a :class:`TermIndex` and a :clsas:`IndexTerm`."""
+        self._state['term_index'] = self.TermIndex()
+        self._state['index_term'] = self.IndexTerm()
+        terms = set(tokens)
+        for index, term in enumerate(terms):
+            self._state['term_index'][term] = index + 1
+            self._state['index_term'][index + 1] = term
 
     def transform(self, tokens: list) -> list:
-        return [self._state['token_index'][token] for token in tokens]
+        """Transform a list of tokens to corresponding indices."""
+        return [self._state['term_index'][token] for token in tokens]
