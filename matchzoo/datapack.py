2758125add458d1158905eee41bdd4899f8b5ad7
bo <kingbolanda@live.com>
2018-07-22 19:14:02 +0200

    update datapack docstring for documentation

--- matchzoo/datapack.py
+++ matchzoo/datapack.py
@@ -4,12 +4,13 @@ import typing
 from pathlib import Path
 
 import dill
+import numpy as np
 import pandas as pd
 
 
 class DataPack(object):
     """
-    Matchzoo DataPack data structure, store dataframe and context.
+    Matchzoo :class:`DataPack` data structure, store dataframe and context.
 
     Example:
         >>> features = [([1,3], [2,3]), ([3,0], [1,6])]
@@ -28,47 +29,55 @@ class DataPack(object):
     DATA_FILENAME = 'data.dill'
 
     def __init__(self,
-                 data: list,
+                 data: typing.Union[list, np.ndarray],
                  context: dict={}):
-        """Initialize."""
+        """
+        Initialize :class:`DataPack`.
+
+        :param data: Input data, could be list-like objects
+                     or :class:`numpy.ndarray`.
+        :param context: Hyper-parameter fitted during
+                        pre-processing stage.
+        """
         self._dataframe = pd.DataFrame(data)
         self._context = context
 
     def __len__(self) -> int:
-        """Get numer of rows in the `DataPack` object."""
+        """Get numer of rows in the class:`DataPack` object."""
         return self._dataframe.shape[0]
 
     @property
     def dataframe(self):
-        """Get data frame."""
+        """Get :meth:`dataframe` of :class:`DataPack`."""
         return self._dataframe
 
     @property
     def context(self):
-        """Get context of `DataPack`."""
+        """Get :meth:`context` of class:`DataPack`."""
         return self._context
 
     def sample(self, number, replace=True):
         """
-        Sample records from `DataPack` object, for generator.
+        Sample records from :class:`DataPack` object, for generator.
 
         :param number: number of records to be sampled, use `batch_size`.
         :param replace: sample with replacement, default value is `True`.
 
-        :return data_pack: return `DataPack` object including sampled data
-                           and context (shallow copy of the context`).
+        :return data_pack: return :class:`DataPack` object including
+                           sampled data and context (shallow copy of
+                           the context`).
         """
         return DataPack(self._dataframe.sample(n=number, replace=replace),
                         self._context.copy())
 
     def append(self, other: 'DataPack'):
         """
-        Append a new `DataPack` object to current `DataPack` object.
+        Append new :class:`DataPack` object to current :class:`DataPack`.
 
-        It should be noted that the context of the previous `DataPack`
+        It should be noted that the context of the previous :class:`DataPack`
         will be updated by the new one.
 
-        :param other: the `DataPack` object to be appended.
+        :param other: the :class:`DataPack` object to be appended.
         """
         other_dataframe = other.dataframe
         other_context = other.context
@@ -79,13 +88,13 @@ class DataPack(object):
 
     def save(self, dirpath: typing.Union[str, Path]):
         """
-        Save the `DataPack` object.
+        Save the :class:`DataPack` object.
 
-        A saved `DataPack` is represented as a directory with a `DataPack`
-        object (transformed user input as features and context), it will be
-        saved by `pickle`.
+        A saved :class:`DataPack` is represented as a directory with a
+        :class:`DataPack` object (transformed user input as features and
+        context), it will be saved by `pickle`.
 
-        :param dirpath: directory path of the saved `DataPack`.
+        :param dirpath: directory path of the saved :class:`DataPack`.
         """
         dirpath = Path(dirpath)
 
@@ -100,10 +109,10 @@ class DataPack(object):
 
 def load_datapack(dirpath: typing.Union[str, Path]) -> DataPack:
     """
-    Load a `DataPack`. The reverse function of :meth:`DataPack.save`.
+    Load a :class:`DataPack`. The reverse function of :meth:`save`.
 
-    :param dirpath: directory path of the saved model
-    :return: a :class:`DataPack` instance
+    :param dirpath: directory path of the saved model.
+    :return: a :class:`DataPack` instance.
     """
     dirpath = Path(dirpath)
 
