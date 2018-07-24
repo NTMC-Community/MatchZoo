b717a5ffde1bc771cebdb44cdb167618096757db
faneshion <fanyixing111@hotmail.com>
2018-07-24 15:49:48 +0800

    first version of base generator

--- matchzoo/engine/base_generator.py
+++ matchzoo/engine/base_generator.py
@@ -1,29 +1,90 @@
 """Base generator."""
 
 import abc
+import keras
+import threading
 
 
-class BaseGenerator(abc.ABCMeta):
-    """
-    Matchzoo generator, accept datapack as input and produce samples for
-    train and test.
-    """
+class BaseGenerator(keras.utils.Sequence):
+    """Matchzoo base generator."""
 
     def __init__(
             self,
-            text_left_max_len: int,
-            text_right_max_len: int,
+            batch_size = int,
+            shuffle: bool,
             is_train: bool
-            ):
+        ):
         """Initialization."""
-        self._text_left_max_len = text_left_max_len
-        self._text_right_max_len = text_right_max_len
+        self.batch_size = batch_size
+        self.shuffle = shuffle
         self.is_train = is_train
+        self.batch_index = 0
+        self.lock = threading.Lock()
+        self.index_array = None
+        self.n = self._total_num_instances()
+        self.index_generator = self._flow_index()
 
     @abc.abstractmethod
-    def get_data() -> tuple:
-        """Get all the data instances."""
+    def _total_num_instances(self):
+        """Return the total number of instances."""
+
+    def _set_index_array(self):
+        self.index_array = np.arange(self.n)
+        if self.shuffle:
+            self.index_array = np.random.permutation(self.n)
+
+    def __getitem__(self, idx):
+        if idx >= len(self):
+            raise ValueError('Asked to retrieve element {idx}, '
+                             'but the Sequence '
+                             'has length {length}'.format(idx=idx,
+                                                          length=len(self)))
+        if self.index_array is None:
+            self._set_index_array()
+        index_array = self.index_array[self.batch_size * idx:
+                                       self.batch_size * (idx + 1)]
+        return self._get_batches_of_transformed_sample(index_array)
+
+    def __len__(self):
+        return (self.n + self.batch_size -1) // self.batch_size # round up
+
+    def on_epoch_end(self):
+        self._set_index_array()
+
+    def reset(self):
+        self.batch_index = 0
+
+    def _flow_index(self):
+        # Ensure self.batch_index is 0.
+        self.reset()
+        while 1:
+            if self.batch_index == 0:
+                self._set_index_array()
+            curr_index = (self.batch_index * self.batch_size) % self.n
+            # note here, some samples may be missed.
+            if self.n > curr_index + self.batch_size:
+                self.batch_index += 1
+            else:
+                self.batch_index = 0
+            yield self.index_array[curr_index: curr_index + self.batch_size]
+
+    def __iter__(self):
+        # Need if we want to do something like:
+        # for x, y in data_gen().flow(...):
+        return self
+
+    def __next__(self, *args, **kwargs):
+        return self.next(*args, **kwargs)
 
     @abc.abstractmethod
-    def get_batch_generator() -> tuple:
-        """Get a generator to produce samples dynamically."""
+    def _get_batches_of_transformed_sample(self, index_array):
+        """Gets a batch of transformed samples.
+
+        # Arguments
+            index_array: Arrray of sample indices to include in batch.
+
+        # Returns
+            A batch of transformed samples.
+
+        """
+        raise NotImplementedError
--- matchzoo/generator/point_generator.py
+++ matchzoo/generator/point_generator.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 from matchzoo import engine
-from matchzoo import DataPack
 
 
 class PointGenerator(BaseGenerator):
@@ -15,53 +14,93 @@ class PointGenerator(BaseGenerator):
             num_class: int=2,
             text_right_max_len: int=5,
             text_left_max_len: int=100,
+            batch_size: int=32,
+            shuffle: bool=True,
             is_train: bool=False
         ):
         """Initialization of point-generator."""
-        super(PointGenerator, self).__init__(text_left_max_len =
-                                             text_left_max_len,
-                                             text_right_max_len =
-                                             text_right_max_len,
-                                             is_train = is_train)
+        self._text_left_max_len = text_left_max_len
+        self._text_right_max_len = text_right_max_len
         self.target_mode = target_mode
         self.num_class = num_class
-        self.features = inputs.dataframe
-        self.context = inputs.context
-        self.fill_word = inputs.context['fill_word']
-        self.num_samples = len(self.features)
+        self.transform_data(inputs)
+        super(PointGenerator, self).__init__(
+                                             batch_size = batch_size,
+                                             shuffle = shuffle,
+                                             is_train = is_train
+                                             )
 
-    def get_data() -> tuple:
+    def transform_data(inputs: DataPack):
+        """Obtain the transformed data from datapack."""
+        data = inputs.dataframe
+        context = inputs.context
+        fill_word = context['fill_word']
+        text_left = []
+        text_right = []
+        labels = []
+        ids = []
+        for idx, sample in enumerate(data.values):
+            x0 = sample[0]
+            x1 = sample[1]
+            y = sample[2]
+            x0_id = sample[3]
+            x1_id = sample[4]
+            ctl_len = min(self.text_left_max_len, len(x0))
+            ctr_len = min(self.text_right_max_len, len(x1))
+            xx0 = np.zeros((self.text_left_max_len,), dtype=np.int32)
+            xx1 = np.zeros((self.text_right_max_len,), dtype=np.int32)
+            xx0[:] = self.fill_word # padding
+            xx1[:] = self.fill_word # padding
+            xx0[:ctl] = x0[:ctl]
+            xx1[:ctr] = x1[:ctr]
+            text_left.append(xx0)
+            text_right.append(xx1)
+            labels.append(y)
+            ids.append((x0_id, x1_id))
+        self.x_left = np.asarray(text_left)
+        self.x_right = np.asarray(text_right)
+        self.y = np.asarray(labels)
+        self.ids = np.asarray(ids)
+
+    def _total_num_instances(self):
+        return len(self.x_left)
+
+    def _get_batches_of_transformed_samples(self, index_array):
         """Get all sampels."""
-        batch_size = self.num_samples
-        t1 = np.zeros((batch_size, self._text_right_max_len), dtype=np.int32)
-        t2 = np.zeros((batch_size, self._text_left_max_len), dtype=np.int32)
-        t1_len = np.zeros((batch_size,), dtype=np.int32)
-        t2_len = np.zeros((batch_size,), dtype=np.int32)
-        t1_ids = []
-        t2_ids = []
+        batch_size = len(index_array)
+        batch_x_left = np.zeros((batch_size, self.text_left_max_len),
+                                dtype=np.int32)
+        batch_x_right = np.zeros((batch_size, self.text_right_max_len),
+                                 dtype=np.int32)
+        batch_ids = []
         if self.target_mode == 'regression':
-            labels = np.zeros((batch_size,), dtype=np.float32)
+            batch_y = np.zeros((batch_size,), dtype=np.float32)
+            batch_y[:] = self.y[index_array]
         elif self.target_mode == 'classification':
-            labels = np.zeros((batch_size, self.num_class), dtype=np.int32)
+            batch_y = np.zeros((batch_size, self.num_class))
+            for i, label in enumerate(self.y[index_array]):
+                batch_y[i, label] = 1
         else:
-            raise ValueError('Error mode for PointGenerator.')
+            raise ValueError('Error target mode in point generator.')
+        for i, j in enumerate(index_array):
+            batch_x_left[i] = self.x_left[j]
+            batch_x_right[i] = self.x_right[j]
+            batch_ids.append(self.ids[j])
 
-        t1[:] = self.fill_word
-        t2[:] = self.fill_word
-        for idx, sample in enumerate(self.features.values):
-            curr_t1_len = min(self._text_right_max_len, len(sample[0]))
-            curr_t2_len = min(self._text_left_max_len, len(sample[1]))
-            t1[, :curr_t1_len] = sample[0][:curr_t1_len]
-            t2[, :curr_t2_len] = sample[1][:curr_t2_len]
-            t1_len[idx] = curr_t1_len
-            t2_len[idx] = curr_t2_len
-            t1_ids.append(sample[2])
-            t2_ids.append(sample[3])
-            labels[idx] = sample[4]
-        return ({'t1': t1, 't2': t2, 't1_len': t1_len, 't2_len': t2_len,
-                     't1_ids': t1_ids, 't2_ids': t2_ids}, labels)
+        return ({'x_left': batch_x_left, 'x_right': batch_x_right, 'ids':
+                 batch_ids}, batch_y)
 
-    def get_batch_generator() -> tuple:
-        """Get a generator to produce samples dynamically."""
-        # to do
+    def next(self):
+        """For python 2.x.
+
+        # Returns
+            The next batch
+        """
+        # Keep under lock only the mechanism which advances
+        # the indexing of each batch.
+        with self.lock:
+            index_array = next(self.index_generator)
+        # The transformation of data is not under thread lock
+        # so it can be done in parallel
+        return self._get_batches_of_transformed_sample(index_array)
 
