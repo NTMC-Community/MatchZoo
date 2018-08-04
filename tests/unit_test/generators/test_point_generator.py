6c41719d811aee892985b50d39e18988a8ac26f1
faneshion <fanyixing111@hotmail.com>
2018-08-05 00:02:53 +0800

    support id_left and id_right for generators

--- tests/unit_test/generators/test_point_generator.py
+++ tests/unit_test/generators/test_point_generator.py
@@ -7,16 +7,16 @@ from matchzoo.datapack import DataPack
 def x():
     data = [
         {'text_left':[1,2,3], 'text_right': [1,2], 'label': 0, \
-        'id': ('did-0', 'did-1')},
+         'id_left': 'did-0', 'id_right': 'did-1'},
         {'text_left':[2,3,4], 'text_right': [3,5], 'label': 1, \
-        'id': ('did-2', 'did-3')}
+         'id_left': 'did-2', 'id_right': 'did-3'}
     ]
     cts = {'vocab_size': 6, 'fill_word': 6}
     return DataPack(data, cts)
 
 @pytest.fixture(scope='module', params=[
     tasks.Classification(num_classes=2),
-    tasks.Ranking()
+    tasks.Ranking(),
 ])
 def task(request):
     return request.param
@@ -32,3 +32,8 @@ def test_point_generator(raw_point_generator):
     x, y = raw_point_generator[0]
     assert x is not None
     assert y is not None
+
+def test_taskmode_in_pointgenerator(x):
+    generator = PointGenerator(x, None, 1, False)
+    with pytest.raises(ValueError):
+        x, y = generator[0]
