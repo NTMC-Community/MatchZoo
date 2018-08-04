739148685783da2bbf04d47a151c9a46179d24c5
bo <kingbolanda@live.com>
2018-08-05 00:12:38 +0200

    update indent

--- tests/inte_test/test_dssm.py
+++ tests/inte_test/test_dssm.py
@@ -7,27 +7,27 @@ from matchzoo import preprocessor
 from matchzoo import models
 
 def prepare_data():
-	"""Prepare train & test data."""
-	train = []
-	test = []
-	path = os.path.dirname(__file__)
-	with open(os.path.join(path, 'train.txt')) as f:
-		train = [tuple(map(str, i.split('\t'))) for i in f]
-	with open(os.path.join(path, 'test.txt')) as f:
-		test = [tuple(map(str, i.split('\t'))) for i in f]
-	return train, test
+    """Prepare train & test data."""
+    train = []
+    test = []
+    path = os.path.dirname(__file__)
+    with open(os.path.join(path, 'train.txt')) as f:
+	train = [tuple(map(str, i.split('\t'))) for i in f]
+    with open(os.path.join(path, 'test.txt')) as f:
+	test = [tuple(map(str, i.split('\t'))) for i in f]
+    return train, test
 
 
 def inte_test_dssm():
-	"""Test DSSM model."""
-	# load data.
-	train, test = prepare_data()
-	# do pre-processing.
-	dssm_preprocessor = preprocessor.DSSMPreprocessor()
-	processed_train = dssm_preprocessor.fit_transform(train, stage='train')
-	# generator.
-	generator = generators.PointGenerator(processed_train)
-	# TODO GENERATOR
+    """Test DSSM model."""
+    # load data.
+    train, test = prepare_data()
+    # do pre-processing.
+    dssm_preprocessor = preprocessor.DSSMPreprocessor()
+    processed_train = dssm_preprocessor.fit_transform(train, stage='train')
+    # generator.
+    generator = generators.PointGenerator(processed_train)
+    # TODO GENERATOR
 	
 
 
