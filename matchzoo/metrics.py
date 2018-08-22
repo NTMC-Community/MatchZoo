f17a4a5f4e9e17396d5769202ea40a699d2e50ca
bo <kingbolanda@live.com>
2018-08-22 16:08:47 +0200

    update doctest

--- matchzoo/metrics.py
+++ matchzoo/metrics.py
@@ -11,8 +11,8 @@ def mean_reciprocal_rank(rs: list) -> float:
 
     Example:
         >>> rs = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
-        >>> mean_reciprocal_rank(rs)
-        0.611111111111111
+        >>> round(mean_reciprocal_rank(rs), 2)
+        0.61
         >>> rs = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0]])
         >>> mean_reciprocal_rank(rs)
         0.5
@@ -36,8 +36,8 @@ def r_precision(r: list) -> float:
 
     Example:
         >>> r = [0, 0, 1]
-        >>> r_precision(r)
-        0.3333333333333333
+        >>> round(r_precision(r), 2)
+        0.33
         >>> r = [0, 1, 0]
         >>> r_precision(r)
         0.5
@@ -71,8 +71,8 @@ def precision_at_k(r: list, k: int) -> float:
         0.0
         >>> precision_at_k(r, 2)
         0.0
-        >>> precision_at_k(r, 3)
-        0.3333333333333333
+        >>> round(precision_at_k(r, 3), 2)
+        0.33
         >>> precision_at_k(r, 4)
         Traceback (most recent call last):
             File "<stdin>", line 1, in ?
@@ -99,8 +99,8 @@ def average_precision(r: list):
 
     Example:
         >>> r = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1]
-        >>> average_precision(r)
-        0.7833333333333333
+        >>> round(average_precision(r), 2)
+        0.78
 
     :param r: Relevance scores (list or numpy) in rank order
               (first element is the first item).
@@ -121,11 +121,11 @@ def mean_average_precision(rs: list) -> float:
 
     Example:
         >>> rs = [[1, 1, 0, 1, 0, 1, 0, 0, 0, 1]]
-        >>> mean_average_precision(rs)
-        0.7833333333333333
+        >>> round(mean_average_precision(rs), 2)
+        0.78
         >>> rs = [[1, 1, 0, 1, 0, 1, 0, 0, 0, 1], [0]]
-        >>> mean_average_precision(rs)
-        0.39166666666666666
+        >>> round(mean_average_precision(rs), 2)
+        0.39
 
     :param rs: Iterator of relevance scores (list or numpy) in rank order
                (first element is the first item).
@@ -148,12 +148,12 @@ def dcg_at_k(r: list, k: int, method: int=0) -> float:
         3.0
         >>> dcg_at_k(r, 2)
         5.0
-        >>> dcg_at_k(r, 2, method=1)
-        4.2618595071429155
-        >>> dcg_at_k(r, 10)
-        9.605117739188811
-        >>> dcg_at_k(r, 11)
-        9.605117739188811
+        >>> round(dcg_at_k(r, 2, method=1), 2)
+        4.26
+        >>> round(dcg_at_k(r, 10), 2)
+        9.61
+        >>> round(dcg_at_k(r, 11), 2)
+        9.61
 
     :param r: Relevance scores (list or numpy) in rank order
               (first element is the first item).
@@ -184,10 +184,10 @@ def ndcg_at_k(r: list, k: int, method: int=0) -> float:
         >>> ndcg_at_k(r, 1)
         1.0
         >>> r = [2, 1, 2, 0]
-        >>> ndcg_at_k(r, 4)
-        0.9203032077642922
-        >>> ndcg_at_k(r, 4, method=1)
-        0.9651954696014428
+        >>> round(ndcg_at_k(r, 4), 2)
+        0.92
+        >>> round(ndcg_at_k(r, 4, method=1), 2)
+        0.97
         >>> ndcg_at_k([0], 1)
         0.0
         >>> ndcg_at_k([1], 2)
