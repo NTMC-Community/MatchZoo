9676f0713f05ab033204b39934e5cd4db1fb5c06
Qinghua Wang <597935261@qq.com>
2018-08-24 18:27:45 +0800

    update log

--- matchzoo/logger.py
+++ matchzoo/logger.py
@@ -1,10 +1,9 @@
 import logging
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s'\
+                        '- %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
