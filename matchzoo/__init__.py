e4399fcf9173c0cca48a5d15dacfd8388255d9ea
Qinghua Wang <597935261@qq.com>
2018-08-24 18:21:01 +0800

    modify log

--- matchzoo/__init__.py
+++ matchzoo/__init__.py
@@ -4,13 +4,4 @@ from . import tasks
 from . import utils
 from .version import __version__
 
-import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
