6befd3528474017c091e73379fad88f0905074c9
ZizhenWang <948280670@qq.com>
2018-09-05 20:51:21 +0800

    Add original loss function in MatchZoo v1.0, and some unit test for it

---
+++ tests/inte_test/test_loss_function.py
@@ -0,0 +1,60 @@
+import os
+import pytest
+import shutil
+import numpy as np
+
+from matchzoo import datapack
+from matchzoo import generators
+from matchzoo.losses import rank_hinge_loss
+from matchzoo import preprocessor
+from matchzoo import models
+from matchzoo import engine
+
+@pytest.fixture
+def train():
+    train = []
+    path = os.path.dirname(__file__)
+    with open(os.path.join(path, 'train.txt')) as f:
+        train = [tuple(map(str, i.split('\t'))) for i in f]
+    return train
+
+@pytest.fixture
+def test():
+    test = []
+    path = os.path.dirname(__file__)
+    with open(os.path.join(path, 'test.txt')) as f:
+        test = [tuple(map(str, i.split('\t'))) for i in f]
+    return test
+
+def test_dssm(train, test):
+    """Test DSSM model."""
+    # do pre-processing.
+    dssm_preprocessor = preprocessor.DSSMPreprocessor()
+    processed_train = dssm_preprocessor.fit_transform(train, stage='train')
+    processed_test = dssm_preprocessor.fit_transform(test, stage='test')
+    # the dimension of dssm model is the length of tri-letters.
+    input_shapes = processed_train.context['input_shapes']
+    # generator.
+    generator = generators.PointGenerator(processed_train, stage='train')
+    # Create a dssm model
+    dssm_model = models.DSSMModel()
+    dssm_model.params['input_shapes'] = input_shapes
+    dssm_model.params['loss'] = rank_hinge_loss()
+    dssm_model.guess_and_fill_missing_params()
+    dssm_model.build()
+    dssm_model.compile()
+    dssm_model.fit_generator(generator)
+    # save
+    dssm_preprocessor.save('.tmpdir')
+    dssm_model.save('.tmpdir')
+
+    # testing
+    dssm_proprecessor = engine.load_preprocessor('.tmpdir')
+    processed_test = dssm_proprecessor.fit_transform(test, stage='test')
+    generator = generators.PointGenerator(processed_test, stage='test')
+    X, y = generator[0]
+    dssm_model = engine.load_model('.tmpdir')
+    predictions = dssm_model.predict([X.text_left, X.text_right])
+    assert len(predictions) > 0
+    assert type(predictions[0][0]) == np.float32
+    shutil.rmtree('.tmpdir')
