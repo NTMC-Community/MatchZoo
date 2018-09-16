aeebcd70d493f062da6deeab8f8f4da01fd076a5
faneshion <fanyixing111@hotmail.com>
2018-09-16 22:11:34 +0800

    test dssm with pointwise & pairwise data. (#348)
    * add test_pair_dssm
    
    * bug in point_generator
    
    * fixture in test_dssm
    
    * add sample folder in tests
    
    * define task for pair_generator

--- matchzoo/generators/point_generator.py
+++ matchzoo/generators/point_generator.py
@@ -85,13 +85,15 @@ class PointGenerator(engine.BaseGenerator):
         # Create label field.
         if self.stage == 'train':
             if isinstance(self._task, tasks.Ranking):
-                batch_y = map(self._task.output_dtype, self._relation['label'])
-                batch_y = np.array(batch_y)
+                self._relation['label'] = self._relation['label'].astype(
+                    self._task.output_dtype)
+                batch_y = self._relation['label'][index_array].values
             elif isinstance(self._task, tasks.Classification):
+                self._relation['label'] = self._relation['label'].astype(
+                    self._task.output_dtype)
                 batch_y = np.zeros((len(index_array), self._task.num_classes))
                 for idx, label in enumerate(
                         self._relation['label'][index_array]):
-                    label = self._task.output_dtype(label)
                     batch_y[idx, label] = 1
             else:
                 msg = f"{self._task} is not a valid task type."
--- tests/inte_test/test_dssm.py
+++ tests/inte_test/test_dssm.py
@@ -8,49 +8,77 @@ from matchzoo import generators
 from matchzoo import preprocessor
 from matchzoo import models
 from matchzoo import engine
+from matchzoo import tasks
 
 @pytest.fixture
 def train():
     train = []
     path = os.path.dirname(__file__)
-    with open(os.path.join(path, 'train.txt')) as f:
-        train = [tuple(map(str, i.split('\t'))) for i in f]
+    with open(os.path.join(path, '../sample/train_rank.txt')) as f:
+        train = [tuple(map(str, i.strip().split('\t'))) for i in f]
     return train
 
 @pytest.fixture
 def test():
     test = []
     path = os.path.dirname(__file__)
-    with open(os.path.join(path, 'test.txt')) as f:
-        test = [tuple(map(str, i.split('\t'))) for i in f]
+    with open(os.path.join(path, '../sample/test_rank.txt')) as f:
+        test = [tuple(map(str, i.strip().split('\t'))) for i in f]
     return test
 
-def test_dssm(train, test):
+@pytest.fixture
+def task(request) -> engine.BaseTask:
+    return tasks.Ranking()
+
+@pytest.fixture
+def dssm_preprocessor():
+    return preprocessor.DSSMPreprocessor()
+
+@pytest.fixture
+def processed_train(train, dssm_preprocessor) -> datapack.DataPack:
+    preprocessed_train = dssm_preprocessor.fit_transform(train, stage='train')
+    dssm_preprocessor.save('.tmpdir')
+    return preprocessed_train
+
+@pytest.fixture
+def processed_test(test) -> datapack.DataPack:
+    dssm_proprecessor = engine.load_preprocessor('.tmpdir')
+    return dssm_proprecessor.fit_transform(test, stage='test')
+
+@pytest.fixture(params=['point', 'pair'])
+def train_generator(request,
+                    processed_train,
+                    task) -> generators.PointGenerator:
+    if request.param == 'point':
+        return generators.PointGenerator(processed_train,
+                                         task=task,
+                                         stage='train')
+    elif request.param == 'pair':
+        return generators.PairGenerator(processed_train,
+                                         stage='train')
+
+@pytest.fixture
+def test_generator(processed_test, task) -> generators.PointGenerator:
+    return generators.PointGenerator(processed_test, task=task, stage='test')
+
+def test_dssm(processed_train,
+              task,
+              train_generator,
+              test_generator):
     """Test DSSM model."""
-    # do pre-processing.
-    dssm_preprocessor = preprocessor.DSSMPreprocessor()
-    processed_train = dssm_preprocessor.fit_transform(train, stage='train')
-    processed_test = dssm_preprocessor.fit_transform(test, stage='test')
-    # the dimension of dssm model is the length of tri-letters.
-    input_shapes = processed_train.context['input_shapes']
-    # generator.
-    generator = generators.PointGenerator(processed_train, stage='train')
     # Create a dssm model
     dssm_model = models.DSSMModel()
-    dssm_model.params['input_shapes'] = input_shapes
+    dssm_model.params['input_shapes'] = processed_train.context['input_shapes']
+    dssm_model.params['task'] = task
     dssm_model.guess_and_fill_missing_params()
     dssm_model.build()
     dssm_model.compile()
-    dssm_model.fit_generator(generator)
+    dssm_model.fit_generator(train_generator)
     # save
-    dssm_preprocessor.save('.tmpdir')
     dssm_model.save('.tmpdir')
 
     # testing
-    dssm_proprecessor = engine.load_preprocessor('.tmpdir')
-    processed_test = dssm_proprecessor.fit_transform(test, stage='test')
-    generator = generators.PointGenerator(processed_test, stage='test')
-    X, y = generator[0]
+    X, y = test_generator[0]
     dssm_model = engine.load_model('.tmpdir')
     predictions = dssm_model.predict([X.text_left, X.text_right])
     assert len(predictions) > 0
