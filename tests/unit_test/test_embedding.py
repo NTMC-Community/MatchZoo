62fb1a33eb3bef58bd59ac6ad4f9d8b034ff0b1a
pangliang <pl8787@qq.com>
2018-09-16 17:43:01 +0800

    [Feature] Add Embedding Unit

--- tests/unit_test/test_embedding.py
+++ tests/unit_test/test_embedding.py
@@ -7,18 +7,18 @@ def term_index_input():
     return {'G': 1, 'C': 2, 'D': 3, 'A': 4, '[PAD]': 0}
 
 def test_embedding(term_index_input):
-    embed = Embedding('tests/unit_test/data/embed_10.txt')
-    embed.process(term_index_input)
+    embed = Embedding('tests/sample/embed_10.txt')
+    embed.build(term_index_input)
     assert embed.dim_embedding == 10
-    assert embed.embedding_file == 'tests/unit_test/data/embed_10.txt'
+    assert embed.embedding_file == 'tests/sample/embed_10.txt'
     assert embed.index_state == {4: 1, 2: 1, 3: 1, 1: 2, 0: 0}
     assert (embed.embedding_mat[0] == np.array([0] * 10)).all()
     assert (np.abs(embed.embedding_mat[2] - \
             (np.array(range(10)) * 0.1 + 0.1)) < 1e-6).all()
 
-    embed = Embedding('tests/unit_test/data/embed_10.txt')
+    embed = Embedding('tests/sample/embed_10.txt')
     del term_index_input['[PAD]']
-    embed.process(term_index_input)
+    embed.build(term_index_input)
     assert embed.dim_embedding == 10
     assert embed.index_state == {4: 1, 2: 1, 3: 1, 1: 2, 0: 0}
     assert (embed.embedding_mat[0] == np.array([0] * 10)).all()
@@ -26,5 +26,5 @@ def test_embedding(term_index_input):
             (np.array(range(10)) * 0.1 + 0.1)) < 1e-6).all()
 
     with pytest.raises(RuntimeError):
-        embed = Embedding('tests/unit_test/data/embed_err.txt.gb2312')
-        embed.process(term_index_input)
+        embed = Embedding('tests/sample/embed_err.txt.gb2312')
+        embed.build(term_index_input)
