5e41229a187f5e32df7c535b161ec1a472a40d33
ZizhenWang <948280670@qq.com>
2018-09-16 23:02:20 +0800

    Revised some codes and doc strings.

--- tests/unit_test/test_losses.py
+++ tests/unit_test/test_losses.py
@@ -14,7 +14,7 @@ def test_hinge_loss():
     expected_loss = (0 + 1 - 0.3 + 0) / 2.0
     loss = K.eval(losses.rank_hinge_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
-    losses._margin = 2
+    losses.margin = 2
     expected_loss = (2 + 0.1 - 1.2 + 2 - 0.3 + 0) / 2.0
     loss = K.eval(losses.rank_hinge_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
@@ -22,8 +22,8 @@ def test_hinge_loss():
                                       [1], [1], [0.8]]))
     pred_value = K.variable(np.array([[1.2], [0.1], [-0.5],
                                       [0], [-2], [-0.3]]))
-    losses._margin = 1
-    losses._neg_num = 2
+    losses.margin = 1
+    losses.neg_num = 2
     expected_loss = (0 + 1 - 0.3 + 0) / 2.0
     loss = K.eval(losses.rank_hinge_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
@@ -31,7 +31,7 @@ def test_hinge_loss():
 
 
 def test_rank_crossentropy_loss():
-    losses._neg_num = 1
+    losses.neg_num = 1
     def softmax(x):
         return np.exp(x)/np.sum(np.exp(x), axis=0)
     true_value = K.variable(np.array([[1.], [0.],
@@ -45,7 +45,7 @@ def test_rank_crossentropy_loss():
                                       [0.], [1.], [0.]]))
     pred_value = K.variable(np.array([[0.8], [0.1], [0.1],
                                       [0.8], [0.1], [0.1]]))
-    losses._neg_num = 2
+    losses.neg_num = 2
     expected_loss = (-np.log(softmax([0.8, 0.1, 0.1])[0])-np.log(softmax([0.8, 0.1, 0.1])[1]))/2
     loss = K.eval(losses.rank_crossentropy_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
