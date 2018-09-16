fd86ac05ad6421ddbe3f6bdbb86f1bf04fb661e9
ZizhenWang <948280670@qq.com>
2018-09-16 22:13:04 +0800

    Revised as advised, add todo item.

--- matchzoo/losses.py
+++ matchzoo/losses.py
@@ -1,6 +1,8 @@
 """Self define loss function."""
 from keras import backend as K
-from keras.layers import Lambda
+from keras import layers
+
+# TODO: Loss function parameters setting.
 
 _margin = 1.
 _neg_num = 1
@@ -8,38 +10,41 @@ _neg_num = 1
 
 def rank_hinge_loss(y_true, y_pred):
     """
-    Support user-defined margin value.
+    Margin and sample-number based ranking loss.
 
     :param y_true: Label.
     :param y_pred: Predict result.
     :return: Hinge loss computed by user-defined margin.
     """
-    y_pos = Lambda(lambda a: a[::2, :], output_shape=(1,))(y_pred)
-    y_neg_list = []
-    for i in range(_neg_num):
-        y_neg_list.append(Lambda(lambda a: a[1::2, :],
-                                 output_shape=(1,))(y_pred))
-    y_neg = K.concatenate(y_neg_list, axis=-1)
+    y_pos = layers.Lambda(lambda a: a[::(_neg_num+1), :],
+                          output_shape=(1,))(y_pred)
+    y_neg = []
+    for neg_idx in range(_neg_num):
+        y_neg.append(layers.Lambda(lambda a: a[(neg_idx+1)::(_neg_num+1), :],
+                                   output_shape=(1,))(y_pred))
+    y_neg = K.max(K.concatenate(y_neg, axis=-1), axis=-1, keepdims=True)
     loss = K.maximum(0., _margin + y_neg - y_pos)
     return K.mean(loss)
 
 
 def rank_crossentropy_loss(y_true, y_pred):
     """
-    Support user-defined negative sample number.
+    Sample-number based crossentropy loss.
 
     :param y_true: Label.
     :param y_pred: Predict result.
     :return: Crossentropy loss computed by user-defined negative number.
     """
-    y_pos_logits = Lambda(lambda a: a[::(_neg_num+1), :])(y_pred)
-    y_pos_labels = Lambda(lambda a: a[::(_neg_num+1), :])(y_true)
-    logits_list, labels_list = [y_pos_logits], [y_pos_labels]
-    for i in range(_neg_num):
-        y_neg_logits = Lambda(lambda a: a[i+1::(_neg_num+1), :])(y_pred)
-        y_neg_labels = Lambda(lambda a: a[i+1::(_neg_num+1), :])(y_true)
-        logits_list.append(y_neg_logits)
-        labels_list.append(y_neg_labels)
+    pos_logits = layers.Lambda(lambda a: a[::(_neg_num+1), :])(y_pred)
+    pos_labels = layers.Lambda(lambda a: a[::(_neg_num+1), :])(y_true)
+    logits_list, labels_list = [pos_logits], [pos_labels]
+    for neg_idx in range(_neg_num):
+        neg_logits = layers.Lambda(lambda a:
+                                   a[neg_idx+1::(_neg_num+1), :])(y_pred)
+        neg_labels = layers.Lambda(lambda a:
+                                   a[neg_idx+1::(_neg_num+1), :])(y_true)
+        logits_list.append(neg_logits)
+        labels_list.append(neg_labels)
     logits = K.concatenate(logits_list, axis=-1)
     labels = K.concatenate(labels_list, axis=-1)
     return -K.mean(K.sum(labels*K.log(K.softmax(logits)), axis=-1))
--- tests/unit_test/test_losses.py
+++ tests/unit_test/test_losses.py
@@ -3,9 +3,7 @@ import pytest
 import numpy as np
 from keras import backend as K
 
-import matchzoo.losses
-from matchzoo.losses import rank_hinge_loss
-from matchzoo.losses import rank_crossentropy_loss
+from matchzoo import losses
 
 
 def test_hinge_loss():
@@ -14,15 +12,26 @@ def test_hinge_loss():
     pred_value = K.variable(np.array([[1.2], [0.1],
                                       [0], [-0.3]]))
     expected_loss = (0 + 1 - 0.3 + 0) / 2.0
-    loss = K.eval(rank_hinge_loss(true_value, pred_value))
+    loss = K.eval(losses.rank_hinge_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
-    matchzoo.losses._margin = 2
+    losses._margin = 2
     expected_loss = (2 + 0.1 - 1.2 + 2 - 0.3 + 0) / 2.0
-    loss = K.eval(rank_hinge_loss(true_value, pred_value))
+    loss = K.eval(losses.rank_hinge_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
+    true_value = K.variable(np.array([[1.2], [1], [0.8],
+                                      [1], [1], [0.8]]))
+    pred_value = K.variable(np.array([[1.2], [0.1], [-0.5],
+                                      [0], [-2], [-0.3]]))
+    losses._margin = 1
+    losses._neg_num = 2
+    expected_loss = (0 + 1 - 0.3 + 0) / 2.0
+    loss = K.eval(losses.rank_hinge_loss(true_value, pred_value))
+    assert np.isclose(expected_loss, loss)
+
 
 
 def test_rank_crossentropy_loss():
+    losses._neg_num = 1
     def softmax(x):
         return np.exp(x)/np.sum(np.exp(x), axis=0)
     true_value = K.variable(np.array([[1.], [0.],
@@ -30,14 +39,13 @@ def test_rank_crossentropy_loss():
     pred_value = K.variable(np.array([[0.8], [0.1],
                                       [0.8], [0.1]]))
     expected_loss = (-np.log(softmax([0.8, 0.1])[0])-np.log(softmax([0.8, 0.1])[1]))/2
-    loss = K.eval(rank_crossentropy_loss(true_value, pred_value))
+    loss = K.eval(losses.rank_crossentropy_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
     true_value = K.variable(np.array([[1.], [0.], [0.],
                                       [0.], [1.], [0.]]))
     pred_value = K.variable(np.array([[0.8], [0.1], [0.1],
                                       [0.8], [0.1], [0.1]]))
-    matchzoo.losses._neg_num = 2
+    losses._neg_num = 2
     expected_loss = (-np.log(softmax([0.8, 0.1, 0.1])[0])-np.log(softmax([0.8, 0.1, 0.1])[1]))/2
-    loss = K.eval(rank_crossentropy_loss(true_value, pred_value))
-    print(loss, expected_loss)
+    loss = K.eval(losses.rank_crossentropy_loss(true_value, pred_value))
     assert np.isclose(expected_loss, loss)
