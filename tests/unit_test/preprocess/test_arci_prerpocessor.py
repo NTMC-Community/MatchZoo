e6b4d0e5179daa717a3bb0897828f99472223f68
pl8787 <pl8787@gmail.com>
2018-10-13 16:41:40 +0800

    [Feature] Add integrate test of ArcI model.

--- tests/unit_test/preprocess/test_arci_prerpocessor.py
+++ tests/unit_test/preprocess/test_arci_prerpocessor.py
@@ -20,23 +20,9 @@ def test_arci_preprocessor_noembed(train_inputs, validation_inputs):
     assert len(rv_train.left) == 1
     assert len(rv_train.right) == 3
     assert len(rv_train.relation) == 3
-    assert sorted(arci_preprocessor._context.keys()) == ['term_index']
-    rv_valid = arci_preprocessor.fit_transform(
-        validation_inputs,
-        stage='test')
-    assert len(rv_valid.left) == 1
-    assert len(rv_valid.right) == 1
-    assert len(rv_valid.relation) == 1
-
-def test_arci_preprocessor_embed(train_inputs, validation_inputs):
-    arci_preprocessor = ArcIPreprocessor("tests/sample/embed_word.txt")
-    rv_train = arci_preprocessor.fit_transform(
-        train_inputs,
-        stage='train')
-    assert len(rv_train.left) == 1
-    assert len(rv_train.right) == 3
-    assert len(rv_train.relation) == 3
-    assert sorted(arci_preprocessor._context.keys()) == ['embedding_mat', 
+    print(rv_train.left)
+    print(rv_train.right)
+    assert sorted(arci_preprocessor._context.keys()) == ['input_shapes',
                                                          'term_index']
     rv_valid = arci_preprocessor.fit_transform(
         validation_inputs,
@@ -44,10 +30,38 @@ def test_arci_preprocessor_embed(train_inputs, validation_inputs):
     assert len(rv_valid.left) == 1
     assert len(rv_valid.right) == 1
     assert len(rv_valid.relation) == 1
+    print(rv_train.left)
+    print(rv_train.right)
+
+def test_arci_preprocessor_embed(train_inputs, validation_inputs):
+    arci_preprocessor = ArcIPreprocessor(
+                            fixed_len=[5, 3],
+                            embedding_file="tests/sample/embed_word.txt"
+                        )
+    rv_train = arci_preprocessor.fit_transform(
+        train_inputs,
+        stage='train')
+    assert len(rv_train.left) == 1
+    assert len(rv_train.right) == 3
+    assert len(rv_train.relation) == 3
+    print(rv_train.left)
+    print(rv_train.right)
+    assert sorted(arci_preprocessor._context.keys()) == ['embedding_mat', 
+                                                         'input_shapes',
+                                                         'term_index']
+    rv_valid = arci_preprocessor.fit_transform(
+        validation_inputs,
+        stage='test')
+    assert len(rv_valid.left) == 1
+    assert len(rv_valid.right) == 1
+    assert len(rv_valid.relation) == 1
+    print(rv_train.left)
+    print(rv_train.right)
 
 def test_arci_preprocessor_embed_err(train_inputs, validation_inputs):
     with pytest.raises(FileNotFoundError):
-        arci_preprocessor = ArcIPreprocessor("tests/sample/embed_word_err.txt")
+        arci_preprocessor = ArcIPreprocessor(
+                                embedding_file="tests/sample/embed_word_err.txt")
         rv_train = arci_preprocessor.fit_transform(
             train_inputs,
             stage='train')
