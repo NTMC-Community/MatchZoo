a5c22e4b4a642cc5e676f9f1db280e618acc16b4
bo <kingbolanda@live.com>
2018-10-19 13:31:16 +0200

    add backend functions for mpm

--- matchzoo/utils/__init__.py
+++ matchzoo/utils/__init__.py
@@ -1,3 +1,7 @@
 from .util import dotdict
-from .util_backend import tensor_mul_tensors
+from .util_backend import (
+    tensor_mul_tensors,
+    tensor_mul_tensors_reduce_dim,
+    tensor_mul_tensors_with_max_pooling
+)
 from .util_preprocessor import validate_context
--- matchzoo/utils/util_backend.py
+++ matchzoo/utils/util_backend.py
@@ -3,12 +3,28 @@
 from keras import layers
 from keras import backend as K
 
-
 def tensor_mul_tensors(tensor, tensors):
 	"""Element wise multiply tensor with list of tensors."""
+	tensors = K.stack(tensors)
+	def element_wise_multiply(current_tensor):
+		"""Do element-wise multiplication."""
+		return layers.multiply([tensor, current_tensor])
+	return K.map_fn(element_wise_multiply, tensors)
+
+def tensor_mul_tensors_reduce_dim(tensor, tensors):
+	"""Element wise multiply tensor with list of tensors, reduce the dimensionality."""
 	tensors = K.stack(tensors)
 	def element_wise_multiply(current_tensor):
 		"""Do element-wise multiplication."""
 		return K.sum(layers.multiply([tensor, current_tensor]),
 		             keepdims=True)
 	return K.map_fn(element_wise_multiply, tensors)
+
+def tensor_mul_tensors_with_max_pooling(tensor, tensors):
+	"""Multiply tensor with list of tensors and retain the maxmum value."""
+	return K.max(tensor_mul_tensors(tensor, tensors),
+	             axis=-1,
+	             keepdims=True)
+	
+
+
