0da1b3984f1e518dffc55ac7d3c2d53ef4bf55cb
uduse <i@uduse.com>
2018-11-08 23:08:49 +0800

    Update context checker by using accessor instead

--- matchzoo/utils/util_preprocessor.py
+++ matchzoo/utils/util_preprocessor.py
@@ -4,7 +4,7 @@
 def validate_context(func):
     """Validate context in the preprocessor."""
     def transform_wrapper(self, *args, **kwargs):
-        if not self._context:
+        if not self.context:
             raise ValueError(
                 'Please fit parameters before transformation.')
         return func(self, *args, **kwargs)
