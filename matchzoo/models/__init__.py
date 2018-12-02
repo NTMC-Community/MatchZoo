ef1c310c705cefe76db36bc28756766b5ce79068
uduse <i@uduse.com>
2018-12-02 11:27:40 +0800

    Add two knrm and convknrm.

--- matchzoo/models/__init__.py
+++ matchzoo/models/__init__.py
@@ -3,3 +3,5 @@ from .dssm_model import DSSMModel
 from .cdssm_model import CDSSMModel
 from .dense_baseline_model import DenseBaselineModel
 from .arci_model import ArcIModel
+from .knrm_model import KNRMModel
+from .conv_knrm_model import ConvKNRMModel
