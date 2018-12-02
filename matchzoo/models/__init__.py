5bf410ddb9285536fcd1e238cd281ccbbc9cdc40
uduse <i@uduse.com>
2018-12-02 12:34:42 +0800

    Add DUETModel

--- matchzoo/models/__init__.py
+++ matchzoo/models/__init__.py
@@ -5,3 +5,4 @@ from .dense_baseline_model import DenseBaselineModel
 from .arci_model import ArcIModel
 from .knrm_model import KNRMModel
 from .conv_knrm_model import ConvKNRMModel
+from .duet_model import DUETModel
