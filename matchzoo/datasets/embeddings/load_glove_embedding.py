9001f6bcc8bde31e975595a6654d61656c008cfc
uduse <i@uduse.com>
2018-12-10 16:18:38 +0800

    Update load_glove_embedding.

--- matchzoo/datasets/embeddings/load_glove_embedding.py
+++ matchzoo/datasets/embeddings/load_glove_embedding.py
@@ -19,7 +19,8 @@ def load_glove_embedding(dimension: int = 50) -> mz.embedding.Embedding:
     """
     ref_path = keras.utils.data_utils.get_file('glove_embedding',
                                                _glove_embedding_url,
-                                               extract=True)
+                                               extract=True,
+                                               cache_dir=mz.USER_DATA_DIR)
     file_name = 'glove.6B.' + str(dimension) + 'd.txt'
     file_path = Path(ref_path).parent.joinpath(file_name)
-    return mz.embedding.load_from_file(file_path=file_path, mode='glove')
+    return mz.embedding.load_from_file(file_path=str(file_path), mode='glove')
