6284f33ed2ee16d426b73642cb1adf7db7bc7b20
uduse <i@uduse.com>
2018-12-10 15:29:39 +0800

    Refactor data pack utils.

---
+++ matchzoo/data_pack/build_vocab_unit.py
@@ -0,0 +1,24 @@
+"""Build a :class:`processor_units.VocabularyUnit` given `data_pack`."""
+from matchzoo import processor_units
+from . import DataPack
+from . import build_unit_from_data_pack
+
+
+def build_vocab_unit(
+    data_pack: DataPack,
+    verbose=1
+) -> processor_units.VocabularyUnit:
+    """
+    Build a :class:`processor_units.VocabularyUnit` given `data_pack`.
+
+    The `data_pack` should be preprocessed forehand, and each item in
+    `text_left` and `text_right` columns of the `data_pack` should be a list
+    of tokens.
+
+    :param data_pack: The :class:`DataPack` to build vocabulary upon.
+    :param verbose: Verbosity.
+    :return: A built vocabulary unit.
+
+    """
+    return build_unit_from_data_pack(processor_units.VocabularyUnit(),
+                                     data_pack, True, verbose)
