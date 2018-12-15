55ae1e3744e280e49b70d721a93498b2715917cc
lixinsu <sulixinict@gmail.com>
2018-12-15 20:58:52 +0800

    add match lstm model

---
+++ matchzoo/models/match_lstm.py
@@ -0,0 +1,78 @@
+"""Match LSTM model."""
+import keras
+import keras.backend as K
+from matchzoo import engine, preprocessors
+
+
+class MLSTMModel(engine.BaseModel):
+    """
+    Match LSTM model.
+
+    Examples:
+        >>> model = MLSTMModel()
+        >>> model.guess_and_fill_missing_params(verbose=0)
+        >>> model.build()
+
+    """
+
+    @classmethod
+    def get_default_preprocessor(cls):
+        """Get default preprocessor."""
+        return preprocessors.NaivePreprocessor()
+
+    @classmethod
+    def get_default_params(cls):
+        """Get default parameters."""
+        params = super().get_default_params(with_embedding=True)
+        params.add(engine.Param(
+            'rnn_hidden_size', 256,
+            hyper_space=engine.hyper_spaces.quniform(low=128, high=384, q=128)
+        ))
+        params.add(engine.Param(
+            'fc_hidden_size', 200,
+            hyper_space=engine.hyper_spaces.quniform(
+                low=100, high=300, q=100)
+        ))
+        params.add(engine.Param('exact_sigma', 0.001))
+        return params
+
+    def build(self):
+        """Build model."""
+        query, doc = self._make_inputs()
+        query_len = query.shape[1]
+        doc_len = doc.shape[1]
+        embedding = self._make_embedding_layer()
+        query_embed = embedding(query)
+        doc_embed = embedding(doc)
+
+        lstm_query = keras.layers.LSTM(self._params['rnn_hidden_size'],
+                                       return_sequences=True)
+        lstm_doc = keras.layers.LSTM(self._params['rnn_hidden_size'],
+                                     return_sequences=True)
+        doc_encoded = lstm_doc(doc_embed)
+        query_encoded = lstm_query(query_embed)
+
+        tensor_left = K.expand_dims(query_encoded, axis=2)
+        tensor_right = K.expand_dims(doc_encoded, axis=1)
+        tensor_left = K.repeat_elements(tensor_left, doc_len, 2)
+        tensor_right = K.repeat_elements(tensor_right, query_len, 1)
+        tensor_merged = K.concatenate([tensor_left, tensor_right], axis=-1)
+        middle_output = keras.layers.Dense(self._params['fc_hidden_size'],
+                                           activation='tanh')(tensor_merged)
+        attn_scores = keras.layers.Dense(1)(middle_output)
+        attn_scores = K.squeeze(attn_scores, axis=3)
+        exp_attn_scores = K.exp(
+            attn_scores - K.max(attn_scores, axis=-1, keepdims=True))
+        exp_sum = K.sum(exp_attn_scores, axis=-1, keepdims=True)
+        attention_weights = exp_attn_scores / exp_sum
+        query_attn_vec = K.batch_dot(attention_weights, doc_encoded)
+
+        concat = keras.layers.Concatenate(axis=2)(
+            [query_attn_vec, query_encoded])
+        lstm_merge = keras.layers.LSTM(self._params['rnn_hidden_size'] * 2,
+                                       return_sequences=True)
+        out = lstm_merge(concat)
+        phi = keras.layers.Dense(self._params['fc_hidden_size'],
+                                 activation='tanh')(out)
+        out = self._make_output_layer()(phi)
+        self._backend = keras.Model(inputs=[query, doc], outputs=[out])
