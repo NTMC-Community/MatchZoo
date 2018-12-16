6e7e6e5ba1339e7143604c8d19698171e3e8e6d9
zhouzhouyang520 <450458747@qq.com>
2018-12-16 02:24:30 -0500

    Format the code of MVLSTM model again.

--- matchzoo/models/mvlstm_model.py
+++ matchzoo/models/mvlstm_model.py
@@ -1,12 +1,8 @@
 """An implementation of MVLSTM Model."""
 import typing
 
-import numpy as np
+import keras
 import keras.backend as K
-from keras.models import Model
-from keras.layers.wrappers import Bidirectional
-from keras.layers import Input, Embedding, Dropout, LSTM, Dot, \
-    Reshape, Lambda
 
 from matchzoo import engine
 
@@ -25,7 +21,7 @@ class MVLSTMModel(engine.BaseModel):
         """:return: model default parameters."""
         params = super().get_default_params(with_embedding=True)
         params['optimizer'] = 'adam'
-        params['input_shapes'] = [(32,), (32,)]
+        params['input_shapes'] = [(5,), (300,)]
         params.add(engine.Param('trainable_embedding', False))
         params.add(engine.Param('vocab_size', 100))
         params.add(engine.Param('hidden_size', 32))
@@ -44,25 +40,25 @@ class MVLSTMModel(engine.BaseModel):
         query, doc = self._make_inputs()
 
         embedding = self._make_embedding_layer()
-        embed_left = embedding(query)
-        embed_right = embedding(doc)
+        embed_query = embedding(query)
+        embed_doc = embedding(doc)
         # Process query & document input.
-        bi_lstm = Bidirectional(LSTM(self._params['hidden_size'],
-                              return_sequences=True,
-                              dropout=self._params['dropout_rate']))
+        bi_lstm = keras.layers.Bidirectional(keras.layers.LSTM(
+            self._params['hidden_size'],
+            return_sequences=True,dropout=self._params['dropout_rate']))
 
-        rep_left = bi_lstm(embed_left)
-        rep_right = bi_lstm(embed_right)
-        cross = Dot(axes=[1, 1], normalize=False)([rep_left, rep_right])
-        cross = Reshape((-1, ))(cross)
+        rep_query = bi_lstm(embed_query)
+        rep_doc = bi_lstm(embed_doc)
 
-        mm_k = Lambda(lambda x: K.tf.nn.top_k(x, 
-                                              k=self._params['top_k'],
-                                              sorted=True)[0]
-                                              )(cross)
-        mm_k = Dropout(rate=self._params['dropout_rate'])(mm_k)
+        matching_matrix = keras.layers.Dot(
+            axes=[1, 1], normalize=False)([rep_query, rep_doc])
+        matching_matrix = keras.layers.Reshape((-1, ))(matching_matrix)
 
-        x_out = self._make_output_layer()(mm_k)
-        self._backend = Model(
-            inputs=[query, doc],
-            outputs=x_out)
+        matching_topk = keras.layers.Lambda(
+            lambda x: K.tf.nn.top_k(x, k=self._params['top_k'], sorted=True)[0]
+        )(matching_matrix)
+        matching_topk = keras.layers.Dropout(
+                    rate=self._params['dropout_rate'])(matching_topk)
+
+        x_out = self._make_output_layer()(matching_topk)
+        self._backend = keras.Model(inputs=[query, doc], outputs=x_out)
