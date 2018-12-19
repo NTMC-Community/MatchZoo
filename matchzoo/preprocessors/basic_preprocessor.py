cb9428fcc99613b6d3a70d2768786e8b10fb643b
faneshion <fanyixing111@hotmail.com>
2018-12-19 10:36:12 +0800

    update parameter setting

--- matchzoo/preprocessors/basic_preprocessor.py
+++ matchzoo/preprocessors/basic_preprocessor.py
@@ -21,6 +21,12 @@ class BasicPreprocessor(engine.BasePreprocessor):
         data_pack.
     :param fixed_length_right: Integer, maximize length of :attr:`right` in the
         data_pack.
+    :param filter_mode: String, mode used by :class:`FrequenceFilterUnit`, Can
+        be 'df', 'cf', and 'idf'.
+    :param filter_low_freq: Float, lower bound value used by
+        :class:`FrequenceFilterUnit`.
+    :param filter_high_freq: Float, upper bound value used by
+        :class:`FrequenceFilterUnit`.
 
     Example:
         >>> import matchzoo as mz
@@ -28,7 +34,10 @@ class BasicPreprocessor(engine.BasePreprocessor):
         >>> test_data = mz.datasets.toy.load_data('test')
         >>> preprocessor = mz.preprocessors.BasicPreprocessor(
         ...     fixed_length_left=10,
-        ...     fixed_length_right=20
+        ...     fixed_length_right=20,
+        ...     filter_mode='df',
+        ...     filter_low_freq=2,
+        ...     filter_high_freq=1000
         ... )
         >>> preprocessor = preprocessor.fit(train_data)
         >>> preprocessor.context['input_shapes']
@@ -45,15 +54,27 @@ class BasicPreprocessor(engine.BasePreprocessor):
     """
 
     def __init__(self, fixed_length_left: int = 30,
-                 fixed_length_right: int = 30):
+                 fixed_length_right: int = 30,
+                 filter_mode: str = 'df',
+                 filter_low_freq: float = 2,
+                 filter_high_freq: float = float('inf')):
         """Initialization."""
         super().__init__()
         self._fixed_length_left = fixed_length_left
         self._fixed_length_right = fixed_length_right
         self._left_fixedlength_unit = processor_units.FixedLengthUnit(
-            self._fixed_length_left, pad_mode='post')
+            self._fixed_length_left,
+            pad_mode='post'
+        )
         self._right_fixedlength_unit = processor_units.FixedLengthUnit(
-            self._fixed_length_right, pad_mode='post')
+            self._fixed_length_right,
+            pad_mode='post'
+        )
+        self._filter_unit = processor_units.FrequencyFilterUnit(
+            low=filter_low_freq,
+            high=filter_high_freq,
+            mode=filter_mode
+        )
 
     def fit(self, data_pack: DataPack, verbose=1):
         """
@@ -61,18 +82,19 @@ class BasicPreprocessor(engine.BasePreprocessor):
 
         :param data_pack: data_pack to be preprocessed.
         :param verbose: Verbosity.
-        :return: class:`DRMMTKSPreprocessor` instance.
+        :return: class:`BasicPreprocessor` instance.
         """
         units = self._default_processor_units()
         data_pack = data_pack.apply_on_text(chain_transform(units))
 
-        filter_unit = processor_units.FrequencyFilterUnit(low=2, mode='df')
-        filter_unit = build_unit_from_data_pack(filter_unit, data_pack,
-                                                flatten=False, mode='right',
-                                                verbose=verbose)
-        data_pack = data_pack.apply_on_text(filter_unit.transform,
+        fitted_filter_unit = build_unit_from_data_pack(self._filter_unit,
+                                                       data_pack,
+                                                       flatten=False,
+                                                       mode='right',
+                                                       verbose=verbose)
+        data_pack = data_pack.apply_on_text(fitted_filter_unit.transform,
                                             mode='right', verbose=verbose)
-        self._context['filter_unit'] = filter_unit
+        self._context['filter_unit'] = fitted_filter_unit
 
         vocab_unit = build_vocab_unit(data_pack, verbose=verbose)
         self._context['vocab_unit'] = vocab_unit
