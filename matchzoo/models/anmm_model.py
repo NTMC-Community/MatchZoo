3d1d5d342aa461271568faff936fa6bd29401abf
yangliuy <yangliuyx@gmail.com>
2018-12-22 23:01:28 -0800

    update anmm

--- matchzoo/models/anmm_model.py
+++ matchzoo/models/anmm_model.py
@@ -29,9 +29,8 @@ class ANMMModel(engine.BaseModel):
         params = super().get_default_params(with_embedding=True)
         params.add(engine.Param('bin_num', 60))
         params.add(engine.Param('dropout_rate', 0.1))
-        params.add(engine.Param('left_text_len', 10))
         params.add(engine.Param('num_layers', 2))
-        params.add(engine.Param('hidden_sizes', [5, 10]))
+        params.add(engine.Param('hidden_sizes', [30, 30]))
         return params
 
     def build(self):
@@ -48,9 +47,11 @@ class ANMMModel(engine.BaseModel):
         q_embed = embedding(query)
         q_attention = keras.layers.Dense(
             1, kernel_initializer=RandomUniform(), use_bias=False)(q_embed)
+        q_text_len = self._params['input_shapes'][0][0]
+
         q_attention = keras.layers.Lambda(
             lambda x: softmax(x, axis=1),
-            output_shape=(self._params['left_text_len'],)
+            output_shape=(q_text_len,)
         )(q_attention)
         d_bin = keras.layers.Dropout(
             rate=self._params['dropout_rate'])(doc)
@@ -59,12 +60,10 @@ class ANMMModel(engine.BaseModel):
                 self._params['hidden_sizes'][layer_id],
                 kernel_initializer=RandomUniform())(d_bin)
             d_bin = keras.layers.Activation('tanh')(d_bin)
-            d_bin = keras.layers.Dense(
-                self._params['hidden_sizes'][self._params['num_layers'] - 1])(
-                d_bin)
-            d_bin = keras.layers.Reshape((self._params['left_text_len'],))(
-                d_bin)
-        q_attention = keras.layers.Reshape((self._params['left_text_len'],))(
-            q_attention)
+        d_bin = keras.layers.Dense(
+            self._params['hidden_sizes'][self._params['num_layers'] - 1])(
+            d_bin)
+        d_bin = keras.layers.Reshape((q_text_len,))(d_bin)
+        q_attention = keras.layers.Reshape((q_text_len,))(q_attention)
         out = keras.layers.Dot(axes=[1, 1])([d_bin, q_attention])
         self._backend = keras.Model(inputs=[query, doc], outputs=out)
