072fae0dae6d199476d98355d2d545fe6128b9e2
bo <kingbolanda@live.com>
2019-01-03 15:11:25 +0100

    add test for base task

--- tests/unit_test/engine/test_base_task.py
+++ tests/unit_test/engine/test_base_task.py
@@ -5,3 +5,6 @@ from matchzoo import engine
 def test_base_task_instantiation():
     with pytest.raises(TypeError):
         engine.BaseTask()
+
+def test_list_available():
+    assert engine.list_available_tasks()
