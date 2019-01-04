17ec18061a9cabfaf8528239e3c4c86fc0936d32
faneshion <fanyixing111@hotmail.com>
2019-01-04 16:20:46 +0800

    add knrm and conv_knrm tutorial

--- matchzoo/models/__init__.py
+++ matchzoo/models/__init__.py
@@ -5,8 +5,8 @@ from .dense_baseline_model import DenseBaselineModel
 from .arci_model import ArcIModel
 from .arcii_model import ArcIIModel
 from .match_pyramid import MatchPyramid
-from .knrm_model import KNRMModel
-from .conv_knrm_model import ConvKNRMModel
+from .knrm_model import KNRM
+from .conv_knrm_model import ConvKNRM
 from .duet import DUET
 from .drmmtks_model import DRMMTKSModel
 from .drmm import DRMM
