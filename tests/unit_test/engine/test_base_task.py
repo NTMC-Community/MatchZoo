123fa94eefe868ba0946af47005efa75f254dbdd
bo <kingbolanda@live.com>
2019-01-05 22:42:16 +0100

    re-organize datapack unit test

--- tests/unit_test/engine/test_base_task.py
+++ tests/unit_test/engine/test_base_task.py
@@ -8,3 +8,7 @@ def test_base_task_instantiation():
 
 def test_list_available():
     assert engine.list_available_tasks()
+
+def test_convert_metrics():
+    assert engine.BaseTask.convert_metrics(None) == []
+    assert engine.BaseTask.convert_metrics('mse') == ['mse']
