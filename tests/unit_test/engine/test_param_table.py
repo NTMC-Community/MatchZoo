71e6e2011318dd4647adaac769dc081863def986
bo <kingbolanda@live.com>
2019-01-05 22:26:21 +0100

    add tests for param table

---
+++ tests/unit_test/engine/test_param_table.py
@@ -0,0 +1,37 @@
+import pytest
+
+from matchzoo.engine import Param, ParamTable
+from matchzoo.engine.hyper_spaces import quniform
+
+@pytest.fixture
+def param_table():
+    params = ParamTable()
+    params.add(Param('ham', 'Parma Ham'))
+    return params
+
+def test_get(param_table):
+    assert param_table['ham'] == 'Parma Ham'
+
+def test_set(param_table):
+    new_param = Param('egg', 'Over Easy')
+    param_table.set('egg', new_param)
+    assert 'egg' in param_table.keys()
+
+def test_keys(param_table):
+    assert 'ham' in param_table.keys()
+
+def test_hyper_space(param_table):
+    new_param = Param(
+        name='my_param',
+        value=1,
+        hyper_space=quniform(low=1, high=5)
+    )
+    param_table.add(new_param)
+    hyper_space = param_table.hyper_space
+    assert hyper_space
+
+    
+
+
+
+    
