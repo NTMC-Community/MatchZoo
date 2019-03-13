27eac2dcb4634243beb7f81bb9f70814e59307f0
ZizhenWang <948280670@qq.com>
2019-03-13 16:57:00 +0800

    #642 update layers

--- matchzoo/layers/dynamic_pooling_layer.py
+++ matchzoo/layers/dynamic_pooling_layer.py
@@ -1,8 +1,8 @@
 """An implementation of Dynamic Pooling Layer."""
 import typing
 
-from keras import backend as K
-from keras.engine import Layer
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
 
 
 class DynamicPoolingLayer(Layer):
@@ -50,16 +50,16 @@ class DynamicPoolingLayer(Layer):
         :param inputs: two input tensors.
         """
         x, dpool_index = inputs
-        dpool_shape = K.tf.shape(dpool_index)
-        batch_index_one = K.tf.expand_dims(
-            K.tf.expand_dims(
-                K.tf.range(dpool_shape[0]), axis=-1),
+        dpool_shape = tf.shape(dpool_index)
+        batch_index_one = tf.expand_dims(
+            tf.expand_dims(
+                tf.range(dpool_shape[0]), axis=-1),
             axis=-1)
-        batch_index = K.tf.expand_dims(
-            K.tf.tile(batch_index_one, [1, self._msize1, self._msize2]),
+        batch_index = tf.expand_dims(
+            tf.tile(batch_index_one, [1, self._msize1, self._msize2]),
             axis=-1)
-        dpool_index_ex = K.tf.concat([batch_index, dpool_index], axis=3)
-        x_expand = K.tf.gather_nd(x, dpool_index_ex)
+        dpool_index_ex = tf.concat([batch_index, dpool_index], axis=3)
+        x_expand = tf.gather_nd(x, dpool_index_ex)
         stride1 = self._msize1 / self._psize1
         stride2 = self._msize2 / self._psize2
 
