40f09f4e75e651e70df399bd57e8c20abbedec7d
caiyinqiong <1198593462@qq.com>
2019-04-03 11:38:53 +0800

    modify variable nameing issue, line formatting and first letter upper case

--- matchzoo/contrib/models/hbmp.py
+++ matchzoo/contrib/models/hbmp.py
@@ -32,11 +32,13 @@ class HBMP(BaseModel):
         params = super().get_default_params(with_embedding=True)
         params['optimizer'] = 'adam'
         params.add(Param(name='alpha', value=0.1,
-                         desc="Negative slope coefficient of LeakyReLU function."))
+                         desc="Negative slope coefficient of LeakyReLU "
+                              "function."))
         params.add(Param(name='mlp_num_layers', value=3,
                          desc="The number of layers of mlp."))
         params.add(Param(name='mlp_num_units', value=[600, 600],
-                         desc="The hidden size of the FC layers, but not include the final layer."))
+                         desc="The hidden size of the FC layers, but not "
+                              "include the final layer."))
         params.add(Param(name='lstm_num_units', value=600,
                          desc="The hidden size of the LSTM layer."))
         params.add(Param(
@@ -55,22 +57,33 @@ class HBMP(BaseModel):
         embed_left = embedding(input_left)
         embed_right = embedding(input_right)
 
-        # get sentence embedding
+        # Get sentence embedding
         embed_sen_left = self._sentence_encoder(
-            embed_left, lstm_num_units=self._params['lstm_num_units'], drop_rate=self._params['dropout_rate'])
+            embed_left,
+            lstm_num_units=self._params['lstm_num_units'],
+            drop_rate=self._params['dropout_rate'])
         embed_sen_right = self._sentence_encoder(
-            embed_right, lstm_num_units=self._params['lstm_num_units'], drop_rate=self._params['dropout_rate'])
+            embed_right,
+            lstm_num_units=self._params['lstm_num_units'],
+            drop_rate=self._params['dropout_rate'])
 
-        # concatenate two sentence embedding: [embed_sen_left, embed_sen_right, |embed_sen_left-embed_sen_right|, embed_sen_left*embed_sen_right]
-        concat1 = keras.layers.Subtract()([embed_sen_left, embed_sen_right])
-        concat1_abs = keras.layers.Lambda(lambda x: abs(x))(concat1)
-        concat2 = keras.layers.Multiply()([embed_sen_left, embed_sen_right])
+        # Concatenate two sentence embedding: [embed_sen_left, embed_sen_right,
+        # |embed_sen_left-embed_sen_right|, embed_sen_left*embed_sen_right]
+        embed_minus = keras.layers.Subtract()(
+            [embed_sen_left, embed_sen_right])
+        embed_minus_abs = keras.layers.Lambda(lambda x: abs(x))(embed_minus)
+        embed_multiply = keras.layers.Multiply()(
+            [embed_sen_left, embed_sen_right])
         concat = keras.layers.Concatenate(axis=1)(
-            [embed_sen_left, embed_sen_right, concat1_abs, concat2])
+            [embed_sen_left, embed_sen_right, embed_minus_abs, embed_multiply])
 
-        # multiply perception layers to classify
-        mlp_out = self._classifier(concat, mlp_num_layers=self._params['mlp_num_layers'], mlp_num_units=self._params[
-            'mlp_num_units'], drop_rate=self._params['dropout_rate'], leaky_relu_alpah=self._params['alpha'])
+        # Multiply perception layers to classify
+        mlp_out = self._classifier(
+            concat,
+            mlp_num_layers=self._params['mlp_num_layers'],
+            mlp_num_units=self._params['mlp_num_units'],
+            drop_rate=self._params['dropout_rate'],
+            leaky_relu_alpah=self._params['alpha'])
         out = self._make_output_layer()(mlp_out)
 
         self._backend = keras.Model(
@@ -83,7 +96,7 @@ class HBMP(BaseModel):
         mlp_num_units: list,
         drop_rate: float,
         leaky_relu_alpah: float
-    )-> typing.Any:
+    ) -> typing.Any:
         for i in range(mlp_num_layers - 1):
             input_ = keras.layers.Dropout(rate=drop_rate)(input_)
             input_ = keras.layers.Dense(mlp_num_units[i])(input_)
@@ -98,24 +111,44 @@ class HBMP(BaseModel):
         drop_rate: float
     ) -> typing.Any:
         """
-        stack three BiLSTM MaxPooling blocks as a hierarchical structure.
-        concatenate the output of three blocs as the input sentence embedding.
-        each BiLSTM layer reads the input sentence as the input.
-        each BiLSTM layer except the first one is initialized(the initial hidden state and the cell state) with the final state of the previous layer.   
+        Stack three BiLSTM MaxPooling blocks as a hierarchical structure.
+        Concatenate the output of three blocs as the input sentence embedding.
+        Each BiLSTM layer reads the input sentence as the input.
+        Each BiLSTM layer except the first one is initialized(the initial
+        hidden state and the cell state) with the final state of the previous
+        layer.
         """
-        layer1 = keras.layers.Bidirectional(keras.layers.LSTM(units=lstm_num_units, return_sequences=True,
-                                                              return_state=True, dropout=drop_rate, recurrent_dropout=drop_rate), merge_mode='concat')(input_)
-        layer1_maxpooling = keras.layers.GlobalMaxPooling1D()(layer1[0])
+        emb1 = keras.layers.Bidirectional(
+            keras.layers.LSTM(
+                units=lstm_num_units,
+                return_sequences=True,
+                return_state=True,
+                dropout=drop_rate,
+                recurrent_dropout=drop_rate),
+            merge_mode='concat')(input_)
+        emb1_maxpooling = keras.layers.GlobalMaxPooling1D()(emb1[0])
 
-        layer2 = keras.layers.Bidirectional(keras.layers.LSTM(units=lstm_num_units, return_sequences=True, return_state=True,
-                                                              dropout=drop_rate, recurrent_dropout=drop_rate), merge_mode='concat')(input_, initial_state=layer1[1:5])
-        layer2_maxpooling = keras.layers.GlobalMaxPooling1D()(layer2[0])
+        emb2 = keras.layers.Bidirectional(
+            keras.layers.LSTM(
+                units=lstm_num_units,
+                return_sequences=True,
+                return_state=True,
+                dropout=drop_rate,
+                recurrent_dropout=drop_rate),
+            merge_mode='concat')(input_, initial_state=emb1[1:5])
+        emb2_maxpooling = keras.layers.GlobalMaxPooling1D()(emb2[0])
 
-        layer3 = keras.layers.Bidirectional(keras.layers.LSTM(units=lstm_num_units, return_sequences=True, return_state=True,
-                                                              dropout=drop_rate, recurrent_dropout=drop_rate), merge_mode='concat')(input_, initial_state=layer2[1:5])
-        layer3_maxpooling = keras.layers.GlobalMaxPooling1D()(layer3[0])
+        emb3 = keras.layers.Bidirectional(
+            keras.layers.LSTM(
+                units=lstm_num_units,
+                return_sequences=True,
+                return_state=True,
+                dropout=drop_rate,
+                recurrent_dropout=drop_rate),
+            merge_mode='concat')(input_, initial_state=emb2[1:5])
+        emb3_maxpooling = keras.layers.GlobalMaxPooling1D()(emb3[0])
 
-        sen_encoder = keras.layers.Concatenate(axis=1)(
-            [layer1_maxpooling, layer2_maxpooling, layer3_maxpooling])
+        emb = keras.layers.Concatenate(axis=1)(
+            [emb1_maxpooling, emb2_maxpooling, emb3_maxpooling])
 
-        return sen_encoder
+        return emb
