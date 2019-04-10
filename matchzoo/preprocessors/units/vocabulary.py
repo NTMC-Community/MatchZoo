d1dfbaa417cb96361ee3a913122ceb6475d310e4
faneshion <fanyixing111@hotmail.com>
2019-04-10 14:12:10 +0800

    <PAD> and <OOV>

--- matchzoo/preprocessors/units/vocabulary.py
+++ matchzoo/preprocessors/units/vocabulary.py
@@ -9,16 +9,16 @@ class Vocabulary(StatefulUnit):
         >>> vocab = Vocabulary()
         >>> vocab.fit(['A', 'B', 'C', 'D', 'E'])
         >>> term_index = vocab.state['term_index']
-        >>> term_index  # doctest: +SKIP
-        {'E': 2, 'C': 3, 'D': 4, 'A': 5, 'B': 6}
+        >>> term_index
+        {'<PAD>': 0, '<OOV>': 1, 'D': 2, 'A': 3, 'B': 4, 'C': 5, 'E': 6}
         >>> index_term = vocab.state['index_term']
-        >>> index_term  # doctest: +SKIP
-        {2: 'C', 3: 'A', 4: 'E', 5: 'B', 6: 'D'}
+        >>> index_term
+        {0: '<PAD>', 1: '<OOV>', 2: 'D', 3: 'A', 4: 'B', 5: 'C', 6: 'E'}
 
         >>> term_index['out-of-vocabulary-term']
         1
         >>> index_term[0]
-        'PAD'
+        '<PAD>'
         >>> index_term[42]
         Traceback (most recent call last):
             ...
@@ -27,25 +27,13 @@ class Vocabulary(StatefulUnit):
         >>> c_index = term_index['C']
         >>> vocab.transform(['C', 'A', 'C']) == [c_index, a_index, c_index]
         True
-        >>> vocab.transform(['C', 'A', 'OOV']) == [c_index, a_index, 1]
+        >>> vocab.transform(['C', 'A', '<OOV>']) == [c_index, a_index, 1]
         True
         >>> indices = vocab.transform(list('ABCDDZZZ'))
         >>> ' '.join(vocab.state['index_term'][i] for i in indices)
-        'A B C D D OOV OOV OOV'
+        'A B C D D <OOV> <OOV> <OOV>'
 
     """
-
-    class IndexTerm(dict):
-        """Map index to term."""
-
-        def __missing__(self, key):
-            """Map out-of-vocabulary indices to empty string."""
-            if key == 1:
-                return 'OOV'
-            elif key == 0:
-                return 'PAD'
-            else:
-                raise KeyError(key)
 
     class TermIndex(dict):
         """Map term to index."""
@@ -57,11 +45,11 @@ class Vocabulary(StatefulUnit):
     def fit(self, tokens: list):
         """Build a :class:`TermIndex` and a :class:`IndexTerm`."""
         self._state['term_index'] = self.TermIndex()
-        self._state['index_term'] = self.IndexTerm()
-        self._state['term_index']['PAD'] = 0
-        self._state['term_index']['OOV'] = 1
-        self._state['index_term'][0] = 'PAD'
-        self._state['index_term'][1] = 'OOV'
+        self._state['index_term'] = dict()
+        self._state['term_index']['<PAD>'] = 0
+        self._state['term_index']['<OOV>'] = 1
+        self._state['index_term'][0] = '<PAD>'
+        self._state['index_term'][1] = '<OOV>'
         terms = set(tokens)
         for index, term in enumerate(terms):
             self._state['term_index'][term] = index + 2
